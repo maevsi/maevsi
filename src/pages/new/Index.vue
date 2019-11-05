29d9111961cbb0977cc1b88eec7e12a5377e68c9
Jonas Thelemann <e-mail@jonas-thelemann.de>
2019-11-05 03:02:28 +0100

    new: Add

---
+++ src/pages/new/Index.vue
@@ -0,0 +1,14 @@
+<template>
+  <Layout>
+    <h1>{{ this.$metaInfo.title }}</h1>
+    <p>It's not yet possible to create new items. Stay tuned!</p>
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo() {
+    return { title: "New Items" };
+  }
+};
+</script>
