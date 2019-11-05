bab3398b2fdeb06ec0d45e7390d55a9cb05acabb
Jonas Thelemann <e-mail@jonas-thelemann.de>
2019-11-05 02:55:24 +0100

    accounts: Add Pages

--- gridsome.server.js
+++ gridsome.server.js
@@ -1,9 +1,5 @@
-// Server API makes it possible to hook into various parts of Gridsome
-// on server-side and add custom data to the GraphQL data layer.
-// Learn more: https://gridsome.org/docs/server-api/
-
+// https://gridsome.org/docs/server-api/
 // Changes here require a server restart.
-// To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = function (api) {
   api.loadSource(({ addCollection }) => {
@@ -12,6 +8,10 @@ module.exports = function (api) {
 
   api.createPages(({ createPage }) => {
     // Use the Pages API here: https://gridsome.org/docs/pages-api/
+    createPage({
+      path: '/account/:account_name([A-Za-z0-9-_]+)',
+      component: './src/templates/Account.vue'
+    }),
     createPage({
       path: '/events/:event_series([A-Za-z0-9-_]+)',
       component: './src/templates/EventSeries.vue'
---
+++ src/templates/Account.vue
@@ -0,0 +1,14 @@
+<template>
+  <Layout>
+    <h1>{{ $route.params.account_name }}</h1>
+    <p>It's not yet possible to view accounts. Stay tuned!</p>
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo() {
+    return { title: this.$route.params.account_name };
+  }
+};
+</script>
