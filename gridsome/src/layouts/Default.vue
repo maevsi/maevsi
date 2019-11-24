0b7de131349cfc2978da8615b21783f482df59a8
Jonas Thelemann <e-mail@jonas-thelemann.de>
2019-11-24 22:40:55 +0100

    gridsome/root: Add Alpha Note
    And move GitHub link to it.

--- gridsome/src/layouts/Default.vue
+++ gridsome/src/layouts/Default.vue
@@ -4,7 +4,6 @@
       <g-link to="/"><g-image alt="Example image" src="~/assets/default-monochrome.svg" width="135" /></g-link>
       <nav class="nav">
         <g-link class="nav__link" to="/account/"><font-awesome :icon="['far', 'user']" />Account</g-link>
-        <a class="nav__link" :href="'https://github.com/' + $static.metadata.siteName + '/' + $static.metadata.siteName + '/'" target="_blank" :title="$static.metadata.siteName + ' on GitHub'"><font-awesome :icon="['fab', 'github']" size="2x" /></a>
       </nav>
     </header>
     <slot/>
--- gridsome/src/pages/Index.vue
+++ gridsome/src/pages/Index.vue
@@ -2,8 +2,20 @@
   <Layout>
     <h1>{{ this.$metaInfo.title }}</h1>
     <p>A manager for events supported by invitees.</p>
+    <p>This site is currently in alpha: anything can change at any time.
+    <br>
+    <a class="nav__link" :href="'https://github.com/' + $static.metadata.siteName + '/' + $static.metadata.siteName + '/'" target="_blank" :title="$static.metadata.siteName + ' on GitHub'"><font-awesome :icon="['fab', 'github']" size="2x" /></a>
+    </p>
   </Layout>
 </template>
+
+<static-query>
+query {
+  metadata {
+    siteName
+  }
+}
+</static-query>
 
 <script>
 export default {
