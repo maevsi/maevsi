b56cad9ceb71ecc6fa3f746b411e5ec401db8da8
Jonas Thelemann <e-mail@jonas-thelemann.de>
2019-11-25 20:12:04 +0100

    gridsome/root: Add GitHub Link Text

--- gridsome/src/pages/Index.vue
+++ gridsome/src/pages/Index.vue
@@ -4,9 +4,11 @@
     <p>A manager for events supported by invitees.</p>
     <p>This site is currently in alpha: anything can change at any time.
     <br>
-    <a :href="'https://github.com/' + $static.metadata.siteName + '/' + $static.metadata.siteName + '/'" target="_blank" :title="$static.metadata.siteName + ' on GitHub'">
-      <font-awesome :icon="['fab', 'github']" size="2x" />
-    </a>
+    <div class="flex justify-center">
+      <a class="items-center flex" :href="'https://github.com/' + $static.metadata.siteName + '/' + $static.metadata.siteName + '/'" target="_blank" :title="$static.metadata.siteName + ' on GitHub'">
+        <font-awesome class="mr-2" :icon="['fab', 'github']" size="2x" /> github.com/maevsi/maevsi
+      </a>
+    </div>
     </p>
     <p>
       <a href="events/"><button class="btn btn-red">Events</button></a>
