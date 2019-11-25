3f3b876291fe9bda881ffcb2fbde720fe328b180
Jonas Thelemann <e-mail@jonas-thelemann.de>
2019-11-25 23:23:20 +0100

    gridsome/root: Correct Tag Placement

--- gridsome/src/pages/Index.vue
+++ gridsome/src/pages/Index.vue
@@ -2,14 +2,13 @@
   <Layout>
     <h1>{{ this.$metaInfo.title }}</h1>
     <p>A manager for events supported by invitees.</p>
-    <p>This site is currently in alpha: anything can change at any time.
+    <p>This site is currently in alpha: anything can change at any time.</p>
     <br>
     <div class="flex justify-center">
       <a class="items-center flex" :href="'https://github.com/' + $static.metadata.siteName + '/' + $static.metadata.siteName + '/'" target="_blank" :title="$static.metadata.siteName + ' on GitHub'">
         <font-awesome class="mr-2" :icon="['fab', 'github']" size="2x" /> github.com/maevsi/maevsi
       </a>
     </div>
-    </p>
     <p>
       <a href="events/"><button class="btn btn-red">Events</button></a>
     </p>
