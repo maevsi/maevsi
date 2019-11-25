8d981d9cea78f6565b9392d3ec271895499994a0
Jonas Thelemann <e-mail@jonas-thelemann.de>
2019-11-25 10:18:28 +0100

    gridsome: Add Apollo

--- gridsome/src/main.js
+++ gridsome/src/main.js
@@ -2,21 +2,36 @@
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
 
 import DefaultLayout from '~/layouts/Default.vue'
+
+import ApolloClient from 'apollo-boost'
+import VueApollo from 'vue-apollo'
 import { config, library } from '@fortawesome/fontawesome-svg-core'
 import { faGithub } from '@fortawesome/free-brands-svg-icons'
 import { faUser } from '@fortawesome/free-regular-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import '@fortawesome/fontawesome-svg-core/styles.css'
 
+const apolloClient = new ApolloClient({
+  uri: 'https://postgraphile.maevsi.test/graphql'
+})
+
+const apolloProvider = new VueApollo({
+  defaultClient: apolloClient,
+})
+
 config.autoAddCss = false;
 library.add(faGithub, faUser)
 
-export default function (Vue, { router, head, isClient }) {
+// export default apolloProvider;
+export default function (Vue, { appOptions, head }) {
   Vue.component('font-awesome', FontAwesomeIcon)
 
   // Set default layout as a global component
   Vue.component('Layout', DefaultLayout)
 
+  Vue.use(VueApollo)
+
+  appOptions.apolloProvider = apolloProvider
   head.htmlAttrs = {class: 'h-full'}
   head.bodyAttrs = {class: 'font-sans'}
   head.meta.push({
