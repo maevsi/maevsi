7827c097cc4093ea28085e0e428fdc56ac321c65
Jonas Thelemann <e-mail@jonas-thelemann.de>
2019-11-25 20:01:17 +0100

    sqitch/function account password change: Underscore Variable Prefix

--- sqitch/deploy/function_account_password_change.sql
+++ sqitch/deploy/function_account_password_change.sql
@@ -10,12 +10,12 @@ BEGIN;
 CREATE FUNCTION maevsi.account_password_change(current_password TEXT, new_password TEXT)
 RETURNS BOOLEAN AS $$
 DECLARE
-  account_current_id maevsi_private.account;
+  _account_current_id maevsi_private.account;
 BEGIN
-  account_current_id := current_setting('jwt.claims.account_id', true)::integer;
-  IF EXISTS (SELECT 1 FROM maevsi_private.account WHERE account.contact_id = account_current_id AND account.password_hash = maevsi.crypt($1, account.password_hash))
+  _account_current_id := current_setting('jwt.claims.account_id', true)::integer;
+  IF EXISTS (SELECT 1 FROM maevsi_private.account WHERE account.contact_id = _account_current_id AND account.password_hash = maevsi.crypt($1, account.password_hash))
   THEN
-    UPDATE maevsi_private.account SET password_hash = maevsi.crypt($2, maevsi.gen_salt('bf')) WHERE account.contact_id = account_current_id;
+    UPDATE maevsi_private.account SET password_hash = maevsi.crypt($2, maevsi.gen_salt('bf')) WHERE account.contact_id = _account_current_id;
     RETURN TRUE;
   ELSE
     RETURN FALSE;
