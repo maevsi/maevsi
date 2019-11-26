7684c052f96eb819e266adb8c459bf827ea5c64c
Jonas Thelemann <e-mail@jonas-thelemann.de>
2019-11-26 05:35:02 +0100

    gridsome/events: Conditional Display

--- gridsome/src/pages/events/Index.vue
+++ gridsome/src/pages/events/Index.vue
@@ -1,22 +1,30 @@
 <template>
   <Layout>
     <h1>{{ this.$metaInfo.title }}</h1>
-    <table class="m-auto">
-      <thead>
-        <tr>
-          <th class="th">Name</th>
-          <th class="th">Date</th>
-          <th class="th">Duration</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="event in allEvents" v-bind:key="event.id">
-          <td class="td"><g-link :to="'/events/' + $slugify(event.username, {lower: true}) + '/' + $slugify(event.name, {lower: true})">{{event.name}}</g-link></td>
-          <td class="td">{{event.timestamp | moment("lll")}}</td>
-          <td class="td">{{event.interval | duration('humanize')}}</td>
-        </tr>
-      </tbody>
-    </table>
+    <div v-if="$apollo.loading">
+      Loading...
+    </div>
+    <div v-else>
+      <table class="m-auto" v-if="allEvents && allEvents.length">
+        <thead>
+          <tr>
+            <th class="th">Name</th>
+            <th class="th">Date</th>
+            <th class="th">Duration</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="event in allEvents" v-bind:key="event.id">
+            <td class="td"><g-link :to="'/events/' + $slugify(event.username, {lower: true}) + '/' + $slugify(event.name, {lower: true})">{{event.name}}</g-link></td>
+            <td class="td">{{event.timestamp | moment("lll")}}</td>
+            <td class="td">{{event.interval | duration('humanize')}}</td>
+          </tr>
+        </tbody>
+      </table>
+      <p v-else>
+        There are currently no events :/
+      </p>
+    </div>
   </Layout>
 </template>
 
@@ -26,7 +34,7 @@ import gql from 'graphql-tag'
 export default {
   data() {
     return {
-      nodes: null
+      allEvents: null
     };
   },
   metaInfo() {
