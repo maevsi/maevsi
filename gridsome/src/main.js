ea789542132f0062efb27d53cf4e21286cef664d
Jonas Thelemann <e-mail@jonas-thelemann.de>
2019-11-28 04:46:35 +0100

    gridsome/main: Add Calendar Day Icon

--- gridsome/src/main.js
+++ gridsome/src/main.js
@@ -12,7 +12,7 @@ import VueMoment from 'vue-moment'
 import { config, library } from '@fortawesome/fontawesome-svg-core'
 import { faGithub } from '@fortawesome/free-brands-svg-icons'
 import { faUser } from '@fortawesome/free-regular-svg-icons'
-import { faKey, faLockOpen, faPlus } from '@fortawesome/free-solid-svg-icons'
+import { faCalendarDay, faKey, faLockOpen, faPlus } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import '@fortawesome/fontawesome-svg-core/styles.css'
 
@@ -37,7 +37,7 @@ const apolloProvider = new VueApollo({
 })
 
 config.autoAddCss = false;
-library.add(faKey, faGithub, faLockOpen, faPlus, faUser)
+library.add(faCalendarDay, faKey, faGithub, faLockOpen, faPlus, faUser)
 
 // export default apolloProvider;
 export default function (Vue, { appOptions, head }) {
