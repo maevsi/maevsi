0d02abd9c72c78764b7e11a8f1186517b062a32f
Jonas Thelemann <e-mail@jonas-thelemann.de>
2019-11-28 08:12:17 +0100

    sqitch: Correct Postgraphile Password Secret

--- sqitch/deploy/role_postgraphile.sql
+++ sqitch/deploy/role_postgraphile.sql
@@ -2,7 +2,7 @@
 
 BEGIN;
 
-\set role_maevsi_postgraphile_password `cat /run/secrets/postgres_role_maevsi-postgraphile`
+\set role_maevsi_postgraphile_password `cat /run/secrets/postgres_role_maevsi-postgraphile_password`
 CREATE ROLE maevsi_postgraphile LOGIN PASSWORD :'role_maevsi_postgraphile_password';
 
 COMMIT;
