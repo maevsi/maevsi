e57d5cdb785ff513aea179193dc68463e560307a
Jonas Thelemann <e-mail@jonas-thelemann.de>
2020-04-04 03:31:25 +0200

    sqitch/rules: Add Comment About "NOT-checks"

--- sqitch/verify/role_anonymous.sql
+++ sqitch/verify/role_anonymous.sql
@@ -5,6 +5,7 @@ BEGIN;
 DO $$
 BEGIN
    ASSERT (SELECT pg_catalog.pg_has_role('maevsi_postgraphile', 'maevsi_anonymous', 'USAGE'));
+   -- Other accounts might not exist yet for a NOT-check.
 END $$;
 
 ROLLBACK;
