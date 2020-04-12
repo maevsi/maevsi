753ba70f160fc4e735e6a524de4513e33584be1b
Jonas Thelemann <e-mail@jonas-thelemann.de>
2020-04-13 00:44:27 +0200

    gridsome/server: Add Path Object

--- gridsome/gridsome.server.js
+++ gridsome/gridsome.server.js
@@ -2,22 +2,24 @@
 // Changes here require a server restart.
 
 module.exports = function (api) {
-  api.loadSource(({ addCollection }) => {
-    // Use the Data Store API here: https://gridsome.org/docs/data-store-api/
-  })
+  const paths = {
+    account: '/accounts/:username([A-Za-z0-9-_]+)',
+    event: '/events/:username([A-Za-z0-9-_]+)/:event_name([-A-Za-z0-9_]+)',
+    eventGroup: '/event-groups/:username([A-Za-z0-9-_]+)/:event_group_name([-A-Za-z0-9_]+)'
+  }
 
   api.createPages(({ createPage }) => {
     // Use the Pages API here: https://gridsome.org/docs/pages-api/
     createPage({
-      path: '/accounts/:username([A-Za-z0-9-_]+)',
+      path: paths.account,
       component: './src/templates/Account.vue'
     })
     createPage({
-      path: '/events/:username([A-Za-z0-9-_]+)/:event_name([-A-Za-z0-9_]+)',
+      path: paths.event,
       component: './src/templates/Event.vue'
     })
     createPage({
-      path: '/event-groups/:username([A-Za-z0-9-_]+)/:event_group_name([-A-Za-z0-9_]+)',
+      path: paths.eventGroup,
       component: './src/templates/EventGroup.vue'
     })
   })
