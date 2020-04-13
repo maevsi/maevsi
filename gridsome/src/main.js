ca232f79b8ce10d8aaa050d299d0e7ccfbc40882
Jonas Thelemann <e-mail@jonas-thelemann.de>
2020-04-13 09:00:16 +0200

    Revert "Remove "GRIDSOME_" Prefix From "STACK_DOMAIN""
    This reverts commit 78a560d99dc28d903807cb54934072e5983a7307.

--- gridsome/src/main.js
+++ gridsome/src/main.js
@@ -20,7 +20,7 @@ import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import '@fortawesome/fontawesome-svg-core/styles.css'
 
 const apolloClient = new ApolloClient({
-  uri: 'https://postgraphile.' + process.env.STACK_DOMAIN + '/graphql',
+  uri: 'https://postgraphile.' + process.env.GRIDSOME_STACK_DOMAIN + '/graphql',
   fetch,
   request: (operation) => {
     global.methods.$jwtDecode((jwt, jwtDecoded) => {
