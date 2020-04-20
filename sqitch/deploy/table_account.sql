974c8bcb31e50a2be4081f232cf5340bfff0c1a0
Jonas Thelemann <e-mail@jonas-thelemann.de>
2020-04-20 20:09:47 +0200

    sqitch/table account: Implement "last_activity" & Add "created"

--- sqitch/deploy/table_account.sql
+++ sqitch/deploy/table_account.sql
@@ -12,7 +12,8 @@ CREATE TABLE maevsi_private.account (
     "email_address"                 TEXT NOT NULL CHECK (char_length("email_address") < 320 AND "email_address" ~* '^.+@.+\..+$') UNIQUE,
     "email_address_verification"    UUID DEFAULT maevsi.uuid_generate_v1mc(),
     "password_hash"                 TEXT NOT NULL,
-    "last_activity"                 TIMESTAMP NOT NULL
+    "created"                       TIMESTAMP NOT NULL DEFAULT NOW(),
+    "last_activity"                 TIMESTAMP NOT NULL DEFAULT NOW(),
 );
 
 COMMENT ON TABLE maevsi_private.account IS 'Account data.';
@@ -21,6 +22,7 @@ COMMENT ON COLUMN maevsi_private.account.username IS 'The account''s username.';
 COMMENT ON COLUMN maevsi_private.account.email_address IS 'The account''s e-mail address for account related information.';
 COMMENT ON COLUMN maevsi_private.account.email_address_verification IS 'The UUID that is used to verify an e-mail address, or null if already verified.';
 COMMENT ON COLUMN maevsi_private.account.password_hash IS 'The account''s password, hashed and salted.';
-COMMENT ON COLUMN maevsi_private.account.last_activity IS 'Timestamp at which the account was last active.';
+COMMENT ON COLUMN maevsi_private.account.created IS 'Timestamp at which the account was last active.';
+COMMENT ON COLUMN maevsi_private.account.last_activity IS 'Timestamp at which the account last requested an access token.';
 
 COMMIT;
