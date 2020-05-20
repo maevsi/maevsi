e4d2eba600300b27b5ca6fa32f49dafaae0ca8fe
Jonas Thelemann <e-mail@jonas-thelemann.de>
2020-05-20 21:08:14 +0200

    gridsome/account-settings: Use Profile Picture Component

--- gridsome/src/templates/AccountSettings.vue
+++ gridsome/src/templates/AccountSettings.vue
@@ -11,12 +11,15 @@
     </div>
     <div v-else>
       <div class="flex flex-col sm:flex-row items-center justify-center min-w-0 py-4">
-        <img
-          alt="blank profile picture"
-          class="flex-none mr-0 sm:mr-4"
-          :src="profilePictureUrl"
-          width="100"
+        <button
+          class="mr-0 sm:mr-4"
+          @click="showModalImageSelection = true"
         >
+          <ProfilePicture
+            ref="profilePicture"
+            :username="this.$route.params.username"
+          />
+        </button>
         <h1 class="mb-0 truncate w-full sm:w-auto">
           {{ $route.params.username }}
         </h1>
@@ -32,13 +35,15 @@
 <script>
 import AlertGraphql from '~/components/AlertGraphql.vue'
 import ImageUploadGallery from '~/components/ImageUploadGallery.vue'
+import ProfilePicture from '~/components/ProfilePicture.vue'
 
 require('@uppy/core/dist/style.css')
 
 export default {
   components: {
     AlertGraphql,
-    ImageUploadGallery
+    ImageUploadGallery,
+    ProfilePicture
   },
   data () {
     return {
