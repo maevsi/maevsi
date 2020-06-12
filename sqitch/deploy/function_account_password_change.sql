eae57753fec8c7899293401d67dd10600b98231c
Jonas Thelemann <e-mail@jonas-thelemann.de>
2020-06-12 02:57:34 +0200

    sqitch/function-account-password-change: Make Void, Raise Errors

--- sqitch/deploy/function_account_password_change.sql
+++ sqitch/deploy/function_account_password_change.sql
@@ -8,17 +8,21 @@
 BEGIN;
 
 CREATE FUNCTION maevsi.account_password_change(password_current TEXT, password_new TEXT)
-RETURNS BOOLEAN AS $$
+RETURNS VOID AS $$
 DECLARE
   _current_username TEXT;
 BEGIN
+  IF char_length($2) < 8
+  THEN
+      RAISE 'New password too short!' USING ERRCODE = 'invalid_parameter_value';
+  END IF;
+
   _current_username := current_setting('jwt.claims.username', true)::TEXT;
   IF EXISTS (SELECT 1 FROM maevsi_private.account WHERE account.username = _current_username AND account.password_hash = maevsi.crypt($1, account.password_hash))
   THEN
     UPDATE maevsi_private.account SET password_hash = maevsi.crypt($2, maevsi.gen_salt('bf')) WHERE account.username = _current_username;
-    RETURN TRUE;
   ELSE
-    RETURN FALSE;
+    RAISE 'Account with given password not found!' USING ERRCODE = 'no_data_found';
   END IF;
 END;
 $$ LANGUAGE PLPGSQL STRICT SECURITY DEFINER;
