1fbee4a197250fe44c86cb84bf204c38b31792bf
Jonas Thelemann <e-mail@jonas-thelemann.de>
2020-11-12 06:32:50 +0100

    refactor(sqitch): alter password change function's description

--- sqitch/deploy/function_account_password_change.sql
+++ sqitch/deploy/function_account_password_change.sql
@@ -18,6 +18,7 @@ BEGIN
   END IF;
 
   _current_username := current_setting('jwt.claims.username', true)::TEXT;
+
   IF EXISTS (SELECT 1 FROM maevsi_private.account WHERE account.username = _current_username AND account.password_hash = maevsi.crypt($1, account.password_hash))
   THEN
     UPDATE maevsi_private.account SET password_hash = maevsi.crypt($2, maevsi.gen_salt('bf')) WHERE account.username = _current_username;
@@ -27,7 +28,7 @@ BEGIN
 END;
 $$ LANGUAGE PLPGSQL STRICT SECURITY DEFINER;
 
-COMMENT ON FUNCTION maevsi.account_password_change(TEXT, TEXT) IS 'Allows to change account passwords.';
+COMMENT ON FUNCTION maevsi.account_password_change(TEXT, TEXT) IS 'Allows to change an account''s password.';
 
 GRANT EXECUTE ON FUNCTION maevsi.account_password_change(TEXT, TEXT) TO maevsi_account;
 
