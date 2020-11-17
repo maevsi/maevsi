66c94c43e439d126af8f970746a24f69a438c330
Jonas Thelemann <e-mail@jonas-thelemann.de>
2020-11-17 05:56:03 +0100

    fix(sqitch): add missing requirement

--- sqitch/deploy/function_account_password_change.sql
+++ sqitch/deploy/function_account_password_change.sql
@@ -1,6 +1,7 @@
 -- Deploy maevsi:function_account_password_change to pg
 -- requires: privilege_execute_revoke
 -- requires: schema_public
+-- requires: schema_private
 -- requires: role_account
 -- requires: table_account
 -- requires: extension_pgcrypto
