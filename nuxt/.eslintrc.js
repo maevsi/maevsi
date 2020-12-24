91e6e5bf3191ff107b74333c9c7258a26a039471
Jonas Thelemann <e-mail@jonas-thelemann.de>
2020-12-24 04:13:19 +0100

    feat: add browserslist

--- nuxt/.eslintrc.js
+++ nuxt/.eslintrc.js
@@ -7,6 +7,7 @@ module.exports = {
     '@nuxtjs/eslint-config-typescript',
     'prettier',
     'prettier/vue',
+    'plugin:compat/recommended',
     'plugin:prettier/recommended',
     'plugin:nuxt/recommended',
   ],
