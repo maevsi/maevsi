d42f4a0eb12d900d61874e09e3c91c0fbec43997
Jonas Thelemann <e-mail@jonas-thelemann.de>
2020-12-27 04:55:45 +0100

    fix(nuxt): correct build module definition

--- nuxt/nuxt.config.js
+++ nuxt/nuxt.config.js
@@ -23,9 +23,9 @@ export default {
 
   // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
   buildModules: [
+    // https://go.nuxtjs.dev/typescript
+    '@nuxt/typescript-build',
     [
-      // https://go.nuxtjs.dev/typescript
-      '@nuxt/typescript-build',
       '@nuxtjs/fontawesome',
       {
         icons: {
