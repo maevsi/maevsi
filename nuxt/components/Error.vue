fe1fc2e1ad23a1a97048e453dd94d0988bbdd2d5
Jonas Thelemann <e-mail@jonas-thelemann.de>
2021-03-13 03:44:57 +0100

    feat(error): improve error 403 information

--- nuxt/components/Error.vue
+++ nuxt/components/Error.vue
@@ -4,8 +4,14 @@
       maev.no? 🥺
     </p>
     <h1>{{ statusCode ? `${statusCode} - ` : '' }}{{ statusReason }}</h1>
+    <p v-if="statusCode === 403" class="text-center">
+      {{ $t('403description') }}
+      <br />
+      {{ $t('403hint') }}
+    </p>
     <ButtonList>
       <ButtonHome />
+      <ButtonEventUnlock v-if="statusCode === 403" />
       <ButtonSignIn v-if="statusCode === 403" />
     </ButtonList>
   </div>
@@ -43,7 +49,11 @@ export default {
 
 <i18n lang="yml">
 de:
+  '403description': 'Du bist aktuell nicht berechtigt, auf diese Seite zuzugreifen.'
+  '403hint': 'Hast du von diesem Gerät aus schon einen Einladungscode eingegeben oder dich angemeldet?'
   'error': 'Fehler'
 en:
+  '403description': "You're currently missing permissions to view this site."
+  '403hint': 'On this device, did you already enter an invitation code or sign in?'
   'error': 'Error'
 </i18n>
