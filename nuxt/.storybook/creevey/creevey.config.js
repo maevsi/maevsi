76733798673ac469c24e2ee209d0bb6e4b2af661
Jonas Thelemann <e-mail@jonas-thelemann.de>
2021-06-07 01:33:36 +0200

    docs(creevey): explain dedicated selenium usage

--- nuxt/.storybook/creevey/creevey.config.js
+++ nuxt/.storybook/creevey/creevey.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  gridUrl: `http://${process.env.HOST_IP || '127.0.0.1'}:4444/wd/hub`,
+  gridUrl: `http://${process.env.HOST_IP || '127.0.0.1'}:4444/wd/hub`, // Always use a dedicated selenium instance, startable by a package.json script, instead of Creevey's own Selenoid for reproducible builds.
 }
