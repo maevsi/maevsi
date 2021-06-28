08b4e0af27089696b07932cf46cbb61a59737a9b
Jonas Thelemann <e-mail@jonas-thelemann.de>
2021-06-29 00:43:19 +0200

    ci(creevey): use host ip in storybook url

--- nuxt/.storybook/creevey/creevey.config.js
+++ nuxt/.storybook/creevey/creevey.config.js
@@ -18,5 +18,5 @@ module.exports = {
     }
   },
   gridUrl: `http://${process.env.HOST_IP || '127.0.0.1'}:4444/wd/hub`, // Always use a dedicated selenium instance, startable by a package.json script, instead of Creevey's own Selenoid for reproducible builds.
-  storybookUrl: `http://${process.env.HOST_IP ? 'storybook:80' : 'localhost:6006'}`,
+  storybookUrl: `http://${process.env.HOST_IP  || '127.0.0.1'}:6006`,
 }
