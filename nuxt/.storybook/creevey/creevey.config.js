5eea228105120595f5e81a05c61e18fb2499cd6d
Jonas Thelemann <e-mail@jonas-thelemann.de>
2021-06-29 07:04:35 +0200

    ci(selenium): use docker compose

--- nuxt/.storybook/creevey/creevey.config.js
+++ nuxt/.storybook/creevey/creevey.config.js
@@ -22,5 +22,5 @@ module.exports = {
   gridUrl: `http://${process.env.HOST_IP || '127.0.0.1'}:4444/wd/hub`, // Always use a dedicated selenium instance, startable by a package.json script, instead of Creevey's own Selenoid for reproducible builds.
   reportDir: path.join(__dirname, './report'),
   screenDir: path.join(__dirname, './images'),
-  storybookUrl: `http://${process.env.HOST_IP || '127.0.0.1'}:6006`,
+  storybookUrl: `http://127.0.0.1:6006`,
 }
