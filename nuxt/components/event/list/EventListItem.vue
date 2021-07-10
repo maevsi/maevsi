b589d8ba3d967afbf102683b9f5937754913c016
Jonas Thelemann <e-mail@jonas-thelemann.de>
2021-07-10 10:42:41 +0200

    fix(event-list-item): correct opacity

--- nuxt/components/event/list/EventListItem.vue
+++ nuxt/components/event/list/EventListItem.vue
@@ -2,7 +2,9 @@
   <li
     class="mb-2 last:mb-0"
     :class="{
-      'opacity-75': $moment(event.end).isBefore($moment()),
+      'opacity-75': event.end
+        ? $moment(event.end).isBefore($moment())
+        : $moment(event.start).isBefore($moment()),
     }"
   >
     <AppLink
@@ -14,33 +16,31 @@
       }"
       :to="localePath('/event/' + event.authorUsername + '/' + event.slug)"
     >
-      <div :class="{ 'opacity-75': $moment(event.start).isBefore($moment()) }">
-        <div class="flex items-center mb-2 text-text-dark text-sm">
-          <div
-            class="font-medium truncate"
-            :class="{
-              'text-green-700': $moment(event.start).isSameOrAfter($moment()),
-            }"
-          >
-            {{ $moment(event.start).format('lll') }}
-          </div>
-        </div>
-        <div class="flex items-center mb-2 text-text-dark text-sm">
-          <EventIconVisibility :event="event" :show-text="false" />
-          <div class="flex items-baseline truncate">
-            <div class="font-bold mx-2 text-xl truncate">
-              {{ event.name }}
-            </div>
-            <Owner :username="event.authorUsername" />
-          </div>
-        </div>
-        <p
-          v-if="event.description"
-          class="line-clamp-2 overflow-ellipsis text-text-dark"
+      <div class="flex items-center mb-2 text-text-dark text-sm">
+        <div
+          class="font-medium truncate"
+          :class="{
+            'text-green-700': $moment(event.start).isSameOrAfter($moment()),
+          }"
         >
-          {{ $htmlToText($md.render(event.description)) }}
-        </p>
+          {{ $moment(event.start).format('lll') }}
+        </div>
       </div>
+      <div class="flex items-center mb-2 text-text-dark text-sm">
+        <EventIconVisibility :event="event" :show-text="false" />
+        <div class="flex items-baseline truncate">
+          <div class="font-bold mx-2 text-xl truncate">
+            {{ event.name }}
+          </div>
+          <Owner :username="event.authorUsername" />
+        </div>
+      </div>
+      <p
+        v-if="event.description"
+        class="line-clamp-2 overflow-ellipsis text-text-dark"
+      >
+        {{ $htmlToText($md.render(event.description)) }}
+      </p>
     </AppLink>
   </li>
 </template>
