74e4d6f05339ce64b4ddadb1eacba9c242d2c98d
Jan Müller <janmueller3698@gmail.com>
2021-08-04 10:40:17 +0200

    feat(vue): use typescript for event components

--- nuxt/types/event.ts
+++ nuxt/types/event.ts
@@ -1,3 +1,15 @@
-export interface Event {
+export type Visibility = 'PUBLIC' | 'PRIVATE'
 
+export interface Event {
+  isArchived: boolean
+  visibility: Visibility
+
+  location: string
+  url?: string
+
+  isInPerson: boolean
+  isRemote: boolean
+
+  start: number
+  end: number
 }
