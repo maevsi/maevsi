a7534c99fe8468bde8ba0fbdd48a1317acea33ae
Jan Müller <janmueller3698@gmail.com>
2021-08-07 16:12:53 +0200

    feat(form-delete): use typescript

--- nuxt/components/form/FormDelete.vue
+++ nuxt/components/form/FormDelete.vue
@@ -15,9 +15,10 @@
   </Form>
 </template>
 
-<script>
+<script lang="ts">
 import { minLength, required } from 'vuelidate/lib/validators'
-import { defineComponent } from '@nuxtjs/composition-api'
+import { defineComponent, PropType } from '@nuxtjs/composition-api'
+import { DocumentNode } from 'graphql'
 
 const consola = require('consola')
 
@@ -25,11 +26,11 @@ export default defineComponent({
   props: {
     itemName: {
       default: undefined,
-      type: String,
+      type: String as PropType<string | undefined>,
     },
     mutation: {
       default: undefined,
-      type: Object,
+      type: Object as PropType<DocumentNode | undefined>,
     },
     update: {
       default: undefined,
@@ -37,16 +38,16 @@ export default defineComponent({
     },
     variables: {
       default: undefined,
-      type: Object,
+      type: Object as PropType<Record<any, any> | undefined>,
     },
   },
   data() {
     return {
       form: {
-        password: undefined,
+        password: undefined as string | undefined,
         sent: false,
       },
-      graphqlError: undefined,
+      graphqlError: undefined as any | undefined,
     }
   },
   methods: {
@@ -59,17 +60,17 @@ export default defineComponent({
 
       this.$apollo
         .mutate({
-          mutation: this.mutation,
+          mutation: this.mutation!!,
           variables: {
             password: this.form.password,
-            ...this.variables,
+            ...(this.variables ?? {}),
           },
-          ...(this.update && { update: this.update }),
+          ...(this.update !== undefined ? { update: this.update } : {}),
         })
         .then((_value) => {
           this.$store.commit('modalAdd', {
             contentBody: this.$global.capitalizeFirstLetter(
-              this.$t('success', { item: this.itemName })
+              this.$t('success', { item: this.itemName }) as string
             ),
             onSubmit: () => this.$emit('success'),
           })
