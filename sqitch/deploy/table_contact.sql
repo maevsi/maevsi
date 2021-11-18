827ac26c9102520aaffbb3f0f99972f037ac2430
Jonas Thelemann <e-mail@jonas-thelemann.de>
2021-11-18 09:47:11 +0100

    fix(contact): allow addition of username only once
    Resolves #477.

--- sqitch/deploy/table_contact.sql
+++ sqitch/deploy/table_contact.sql
@@ -16,7 +16,8 @@ CREATE TABLE maevsi.contact (
   first_name                 TEXT CHECK (char_length(first_name) > 0 AND char_length(first_name) < 100),
   last_name                  TEXT CHECK (char_length(last_name) > 0 AND char_length(last_name) < 100),
   phone_number               TEXT CHECK (phone_number ~ '^\+(9[976]\d|8[987530]\d|6[987]\d|5[90]\d|42\d|3[875]\d|2[98654321]\d|9[8543210]|8[6421]|6[6543210]|5[87654321]|4[987654310]|3[9643210]|2[70]|7|1)\d{1,14}$'),
-  "url"                      TEXT CHECK (char_length("url") < 300 AND "url" ~ '^https:\/\/')
+  "url"                      TEXT CHECK (char_length("url") < 300 AND "url" ~ '^https:\/\/'),
+  UNIQUE (author_account_username, account_username)
 );
 
 COMMENT ON TABLE maevsi.contact IS 'Contact data.';
