e78cbe84f863047bee92e2ea8054469510be5c7e
Jonas Thelemann <e-mail@jonas-thelemann.de>
2021-12-07 00:59:40 +0100

    feat: improve base components

--- nuxt/components/card/Card.vue
+++ nuxt/components/card/Card.vue
@@ -7,7 +7,7 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from '@nuxtjs/composition-api'
+import { defineComponent } from '#app'
 
 export default defineComponent({
   name: 'MaevsiCard',
