452b6a754109fe417c48b994c7fd8057139ff8f5
Jonas Thelemann <e-mail@jonas-thelemann.de>
2022-02-17 03:25:32 +0100

    fix(icon-menu): remove incorrect icon tag

--- nuxt/components/icon/IconMenu.vue
+++ nuxt/components/icon/IconMenu.vue
@@ -1,20 +1,18 @@
 <template>
-  <Icon>
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      :class="classes"
-      fill="none"
-      viewBox="0 0 24 24"
-      stroke="currentColor"
-    >
-      <path
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        stroke-width="2"
-        d="M4 6h16M4 12h16M4 18h16"
-      />
-    </svg>
-  </Icon>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    :class="classes"
+    fill="none"
+    viewBox="0 0 24 24"
+    stroke="currentColor"
+  >
+    <path
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      stroke-width="2"
+      d="M4 6h16M4 12h16M4 18h16"
+    />
+  </svg>
 </template>
 
 <script lang="ts">
