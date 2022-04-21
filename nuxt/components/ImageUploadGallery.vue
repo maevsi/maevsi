0eba08a87d16718918709c0455e9732b80fd0168
Jonas Thelemann <e-mail@jonas-thelemann.de>
2022-04-21 09:38:21 +0200

    fix(image-upload-gallery): add missing classes

--- nuxt/components/ImageUploadGallery.vue
+++ nuxt/components/ImageUploadGallery.vue
@@ -57,7 +57,7 @@
             </div>
           </li>
         </template>
-        <li>
+        <li class="relative box-border border-4 border-transparent">
           <Button
             v-if="allowAddition"
             :aria-label="
