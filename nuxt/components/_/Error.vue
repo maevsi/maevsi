816208219fd334ced490dc561ec834009e6459d6
Jonas Thelemann <e-mail@jonas-thelemann.de>
2022-07-14 23:24:41 +0200

    feat(twemoji): use svg instead

--- nuxt/components/_/Error.vue
+++ nuxt/components/_/Error.vue
@@ -1,10 +1,14 @@
 <template>
   <div class="flex flex-col gap-4">
     <!-- eslint-disable vue/no-v-html -->
-    <p
-      class="text-center text-xl font-black"
-      v-html="twemoji.parse($t('maevNo'), { className: 'h-5 inline-block' })"
-    />
+    <i18n
+      path="maevNo"
+      class="flex items-center justify-center gap-1 text-xl font-black"
+    >
+      <template slot="emojiFacePleading">
+        <EmojiFacePleading />
+      </template>
+    </i18n>
     <!--eslint-enable-->
     <h1>{{ statusCode ? `${statusCode} - ` : '' }}{{ statusReason }}</h1>
     <p v-if="statusCode === 403" class="text-center">
@@ -21,7 +25,6 @@
 
 <script lang="ts">
 import { status } from '@http-util/status-i18n'
-import twemoji from 'twemoji'
 
 import { defineComponent, PropType } from '#app'
 
@@ -32,11 +35,6 @@ export default defineComponent({
       default: undefined,
       type: Number as PropType<number | undefined>,
     },
-  },
-  data() {
-    return {
-      twemoji,
-    }
   },
   computed: {
     statusReason(): string {
@@ -54,10 +52,10 @@ de:
   403description: Du bist aktuell nicht berechtigt, auf diese Seite zuzugreifen.
   403hint: Hast du dich schon angemeldet oder von diesem Gerät aus einen Einladungscode eingegeben?
   error: Fehler
-  maevNo: maev.no? 🥺
+  maevNo: maev.no? {emojiFacePleading}
 en:
   403description: "You're currently missing permissions to view this site."
   403hint: Did you already sign in or enter an invitation code on this device?
   error: Error
-  maevNo: maev.no? 🥺
+  maevNo: maev.no? {emojiFacePleading}
 </i18n>
