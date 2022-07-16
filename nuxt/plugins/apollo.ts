4dda1eeecb28903eaf75f1e606d2574e9498185a
Jonas Thelemann <e-mail@jonas-thelemann.de>
2022-07-16 04:04:08 +0200

    feat(apollo): use ssr

--- nuxt/plugins/apollo.ts
+++ nuxt/plugins/apollo.ts
@@ -8,22 +8,39 @@ import VueApollo from 'vue-apollo'
 
 Vue.use(VueApollo)
 
-const httpLink = createHttpLink({
-  uri: process.server
-    ? 'http://postgraphile:5000/graphql'
-    : 'https://postgraphile.' +
-      (process.env.NUXT_ENV_STACK_DOMAIN || 'maevsi.test') +
-      '/graphql',
-})
-const cache = new InMemoryCache()
-const apolloClient = new ApolloClient({
-  link: httpLink,
-  cache,
-})
-const apolloProvider = new VueApollo({
-  defaultClient: apolloClient,
-})
-
 export default (_: Context, inject: Inject) => {
+  const httpLink = createHttpLink({
+    uri: process.server
+      ? 'http://postgraphile:5000/graphql'
+      : 'https://postgraphile.' +
+        (process.env.NUXT_ENV_STACK_DOMAIN || 'maevsi.test') +
+        '/graphql',
+  })
+  const cache = new InMemoryCache()
+
+  if (process.client) {
+    if (typeof window !== 'undefined') {
+      const state = window.__APOLLO_STATE__
+      if (state) {
+        cache.restore(state.defaultClient)
+      }
+    }
+  }
+
+  const apolloClient = new ApolloClient({
+    link: httpLink,
+    cache,
+    ...(process.server
+      ? {
+          ssrMode: true,
+        }
+      : {
+          ssrForceFetchDelay: 100,
+        }),
+  })
+  const apolloProvider = new VueApollo({
+    defaultClient: apolloClient,
+  })
+
   inject('apolloProvider', apolloProvider)
 }
