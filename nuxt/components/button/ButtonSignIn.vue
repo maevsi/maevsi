53886289c6f963cdcae775b89305a1513443d922
Jonas Thelemann <e-mail@jonas-thelemann.de>
2022-08-22 16:02:53 +0200

    chore(button-sign-in): remove unused query navigation

--- nuxt/components/button/ButtonSignIn.vue
+++ nuxt/components/button/ButtonSignIn.vue
@@ -1,18 +1,7 @@
 <template>
   <ButtonColored
     :aria-label="$t('signIn')"
-    :to="
-      localePath({
-        path: '/task/account/sign-in',
-        query: {
-          tab: 'signIn',
-          ...(isReferring && {
-            referrer: $route.fullPath,
-            isRedirectNoticeHidden: true,
-          }),
-        },
-      })
-    "
+    :to="localePath({ path: '/task/account/sign-in' })"
     @click="$emit('click')"
   >
     {{ $t('signIn') }}
@@ -21,19 +10,6 @@
     </template>
   </ButtonColored>
 </template>
-
-<script lang="ts">
-import { defineComponent } from '#app'
-
-export default defineComponent({
-  props: {
-    isReferring: {
-      default: true,
-      type: Boolean,
-    },
-  },
-})
-</script>
 
 <i18n lang="yml">
 de:
