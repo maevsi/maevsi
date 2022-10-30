0a111cfc3d5d99dd0ab3ab5f49657b56e939a015
Omar Goatfryed Sood <goatfryed@gmail.com>
2022-10-30 22:13:28 +0100

    chore: removed unused function
    awesome name, but not required :(

--- nuxt/components/form/input/dateTimeInputUtil.ts
+++ nuxt/components/form/input/dateTimeInputUtil.ts
@@ -3,16 +3,6 @@ import { Ref } from 'vue'
 
 const DEFAULT_FORMAT = DateTime.DATETIME_SHORT
 
-export function stringcronizedDate(ref: Ref<string | undefined>) {
-  return computed<DateTime | undefined>({
-    get: () =>
-      ref.value !== undefined ? DateTime.fromISO(ref.value) : undefined,
-    set: (dt: DateTime | undefined) => {
-      ref.value = dt !== undefined ? dt.toISO() : undefined
-    },
-  })
-}
-
 export function useDateTimeInput(value: Ref<DateTime | undefined>) {
   return {
     value,
