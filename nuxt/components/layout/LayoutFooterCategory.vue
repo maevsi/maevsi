57a7798a1ddfabfa2d54b9e060f5c0acbc8c45c0
Jonas Thelemann <e-mail@jonas-thelemann.de>
2022-11-10 23:23:26 +0100

    fix(footer): add gap

--- nuxt/components/layout/LayoutFooterCategory.vue
+++ nuxt/components/layout/LayoutFooterCategory.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="flex flex-1 flex-col items-start p-4 basis-1/2 md:flex-none md:basis-auto"
+    class="flex flex-1 flex-col items-start p-4 basis-1/2 md:flex-none md:basis-auto gap-2"
   >
     <span class="whitespace-nowrap text-lg font-medium leading-7">
       {{ heading }}
