89ad2705007406dfe487feacea118921926f2d99
Jonas Thelemann <e-mail@jonas-thelemann.de>
2022-11-16 13:57:54 +0100

    fix(loader-indicator): correct display behavior

---
+++ nuxt/components/loader/indicator/LoaderIndicator.vue
@@ -0,0 +1,14 @@
+<template>
+  <div
+    class="flex h-full items-center justify-center"
+    :title="t('globalLoading')"
+  >
+    <div class="flex items-center justify-center w-1/2">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+const { t } = useI18n()
+</script>
