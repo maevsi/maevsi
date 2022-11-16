d582edad0b01cdc6ddf642581c63709aac93e225
Jonas Thelemann <e-mail@jonas-thelemann.de>
2022-11-16 05:07:00 +0100

    chore(test): move cypress directory

--- nuxt/cypress.config.ts
+++ nuxt/cypress.config.ts
@@ -4,6 +4,7 @@ import getCompareSnapshotsPlugin from 'cypress-visual-regression/dist/plugin'
 import { CYPRESS_BASE_URL } from './utils/constants'
 
 export default defineConfig({
+  downloadsFolder: 'test/cypress/downloads',
   e2e: {
     baseUrl: CYPRESS_BASE_URL,
     setupNodeEvents(on, config) {
@@ -16,11 +17,16 @@ export default defineConfig({
 
       return config
     },
+    supportFile: 'test/cypress/support/e2e.{js,jsx,ts,tsx}',
+    specPattern: 'test/cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
     video: false,
   },
   env: {
     failSilently: false,
   },
-  screenshotsFolder: './cypress/snapshots/actual',
+  fixturesFolder: 'test/cypress/fixtures',
+  screenshotsFolder: 'test/cypress/snapshots/actual',
+  supportFolder: 'test/cypress/support',
   trashAssetsBeforeRuns: true,
+  videosFolder: 'test/cypress/videos',
 })
