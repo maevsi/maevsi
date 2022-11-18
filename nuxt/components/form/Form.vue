309a2487e60822dd03ab96a968266fbb733fa6d3
Jonas Thelemann <e-mail@jonas-thelemann.de>
2022-11-18 03:57:19 +0100

    fix(form): correct card height

--- nuxt/components/form/Form.vue
+++ nuxt/components/form/Form.vue
@@ -8,7 +8,7 @@
     novalidate
     @submit="(e) => emit('submit', e)"
   >
-    <Card class="flex flex-col">
+    <Card class="flex flex-col" is-high>
       <div class="flex flex-col min-h-0 overflow-y-auto gap-6">
         <slot />
         <div class="flex flex-col items-center justify-between">
