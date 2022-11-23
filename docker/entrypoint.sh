853ae68653b1e0df4b59f983bb7178997027d3e9
Jonas Thelemann <e-mail@jonas-thelemann.de>
2022-11-23 23:56:21 +0100

    chore: use node 19 & pnpm

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -5,7 +5,7 @@ sqitch -C /srv/sqitch/ deploy "$(cat /run/secrets/maevsi_sqitch-target)"
 
 if [ "$NODE_ENV" != "production" ]; then
     pnpm config set store-dir "/srv/.pnpm-store"
-    pnpm i
+    pnpm install
 fi
 
 exec "$@"
