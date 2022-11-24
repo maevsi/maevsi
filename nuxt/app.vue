bf8236ddccb669032dde154080f2627696b7d8d7
Jonas Thelemann <e-mail@jonas-thelemann.de>
2022-11-24 05:15:07 +0100

    feat(app): remove migration notice

--- nuxt/app.vue
+++ nuxt/app.vue
@@ -1,23 +1,5 @@
 <template>
   <div>
-    <CardStateInfo is-edgy>
-      <i18n-t keypath="infoEngine" tag="span">
-        <template #move>
-          <AppLink is-underlined :is-colored="false" to="https://nuxt.com/v3">
-            {{ t('infoEngineMove') }}
-          </AppLink>
-        </template>
-        <template #tell>
-          <AppLink
-            is-underlined
-            :is-colored="false"
-            to="mailto:mail+support@maev.si"
-          >
-            {{ t('infoEngineTell') }}
-          </AppLink>
-        </template>
-      </i18n-t>
-    </CardStateInfo>
     <CardStateInfo v-if="!isBrowserSupported" is-edgy>
       {{ t('browserUnsupported') }}
     </CardStateInfo>
@@ -45,12 +27,6 @@ onBeforeMount(() => {
 <i18n lang="yaml">
 de:
   browserUnsupported: Dein Browser scheint veraltet zu sein. Manche Dinge könnten deshalb nicht funktionieren oder komisch aussehen.
-  infoEngine: Maevsi ist {move}! 🚚 Wenn etwas kaputt gegangen ist, {tell}.
-  infoEngineMove: umgezogen
-  infoEngineTell: sag Bescheid
 en:
   browserUnsupported: Your browser version seems outdated. Some things might not work as expected or look funny.
-  infoEngine: Maevsi {move}! 🚚 If something broke, {tell}.
-  infoEngineMove: moved
-  infoEngineTell: please tell us
 </i18n>
