da03164318f22acaa68d46a2a4fcd3e18e135092
Jonas Thelemann <e-mail@jonas-thelemann.de>
2022-12-15 02:53:00 +0100

    fix(dayjs): workaround ssr issue
    Resolves #956.

--- nuxt/plugins/moment.ts
+++ nuxt/plugins/moment.ts
@@ -1,5 +1,9 @@
 import moment from 'dayjs'
-import 'dayjs/locale/de'
+
+// workaround for [1]
+import de from 'dayjs/locale/de'
+// import 'dayjs/locale/de' does not make locale available
+
 import duration from 'dayjs/plugin/duration'
 import isSameOrAfter from 'dayjs/plugin/isSameOrAfter'
 import localizedFormat from 'dayjs/plugin/localizedFormat'
@@ -11,9 +15,20 @@ export default defineNuxtPlugin((_nuxtApp) => {
   moment.extend(localizedFormat)
   moment.extend(relativeTime)
 
+  // workaround for [1]
+  moment.locale(de)
+  // moment.locale(en) makes `format` error
+
   return {
     provide: {
       moment,
     },
   }
 })
+
+/*
+  [1]
+  https://github.com/nuxt/framework/issues/7534#issuecomment-1248596609
+  https://github.com/nuxt/framework/issues/7206
+  https://github.com/maevsi/maevsi/issues/956
+*/
