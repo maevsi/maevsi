7c79cba44442bb002d52f3178dd80ad7875e4000
Jonas Thelemann <e-mail@jonas-thelemann.de>
2023-01-30 18:52:46 +0100

    fix(headers): set hsts only for production

--- nuxt/server/middleware/headers.ts
+++ nuxt/server/middleware/headers.ts
@@ -24,15 +24,18 @@ export default defineEventHandler(async (event) => {
     'Report-To',
     '\'{"group":"default","max_age":31536000,"endpoints":[{"url":"https://dargmuesli.report-uri.com/a/d/g"}],"include_subdomains":true}\''
   )
-  appendHeader(
-    event,
-    'Strict-Transport-Security',
-    'max-age=31536000; includeSubDomains; preload'
-  )
   appendHeader(event, 'X-Content-Type-Options', 'nosniff')
   appendHeader(event, 'X-DNS-Prefetch-Control', 'off')
   appendHeader(event, 'X-Download-Options', 'noopen')
   appendHeader(event, 'X-Frame-Options', 'SAMEORIGIN')
   appendHeader(event, 'X-Permitted-Cross-Domain-Policies', 'none')
   appendHeader(event, 'X-XSS-Protection', '1; mode=block') // TODO: set back to `0` once CSP does not use `unsafe-*` anymore (https://github.com/maevsi/maevsi/issues/1047)
+
+  if (process.env.NODE_ENV === 'production') {
+    appendHeader(
+      event,
+      'Strict-Transport-Security',
+      'max-age=31536000; includeSubDomains; preload'
+    )
+  }
 })
