8288e622b46a3a02371da2f4b7dc0104bd58faf0
Jonas Thelemann <e-mail@jonas-thelemann.de>
2023-04-14 04:36:27 +0200

    fix(account): run email verification on client only

--- nuxt/types/types.ts
+++ nuxt/types/types.ts
@@ -4,7 +4,7 @@ import { GraphQLError } from 'graphql'
 export type ArrayElement<ArrayType extends readonly unknown[]> =
   ArrayType extends readonly (infer ElementType)[] ? ElementType : never
 
-// if an `originalError` property shows up, see the PR's changes for https://github.com/maevsi/maevsi/issues/993
+// if an `originalError` property seems to be required, consider https://github.com/maevsi/maevsi/pull/1182/files#diff-3011f5fc2745796b181fe633b2590cfeb2e976aec95b8e94630bba0cc6163c1cL8
 export type BackendError = {
   graphQLErrors: (GraphQLError & { errcode?: string })[]
 } & CombinedError
