2d17ccd8f088ce6b4737dce4b8c1d2d88b3c85e6
Jonas Thelemann <e-mail@jonas-thelemann.de>
2023-04-20 02:45:16 +0200

    fix(event-dashlet-start): remove icon used for testing

--- nuxt/components/event/dashlet/EventDashletStart.vue
+++ nuxt/components/event/dashlet/EventDashletStart.vue
@@ -1,7 +1,6 @@
 <template>
   <EventDashlet v-if="event.start">
     <IconCalendar :title="t('start')" />
-    <IconX />
     <div class="flex flex-col">
       <span>
         {{ eventStart.format('lll') }}
