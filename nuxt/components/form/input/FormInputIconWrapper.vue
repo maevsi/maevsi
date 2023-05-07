9bc0a3e5eebe896ce7d263609945a68bc1a325e1
Jonas Thelemann <e-mail@jonas-thelemann.de>
2023-05-07 02:13:36 +0200

    fix(form-input-icon-wrapper): simplify

--- nuxt/components/form/input/FormInputIconWrapper.vue
+++ nuxt/components/form/input/FormInputIconWrapper.vue
@@ -1,7 +1,5 @@
 <template>
-  <span class="absolute right-0 top-1/2 -translate-y-1/2 px-3">
-    <div class="rounded-full bg-gray-50">
-      <slot />
-    </div>
+  <span class="absolute right-0 top-1/2 -translate-y-1/2 pr-3">
+    <slot />
   </span>
 </template>
