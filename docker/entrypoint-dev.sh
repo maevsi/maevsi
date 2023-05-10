7dd1b1576e707dc38f2433586fe5b3898cc65012
Jonas Thelemann <e-mail@jonas-thelemann.de>
2023-05-10 05:35:12 +0200

    feat(nuxt): strip sqitch from Dockerfile

--- docker/entrypoint-dev.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-set -e
-
-pnpm config set store-dir /srv/.pnpm-store
-pnpm install
-
-exec "$@"
--- nuxt/docker-entrypoint.sh
+++ nuxt/docker-entrypoint.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 set -e
 
-sqitch -C /srv/app/sqitch/ deploy "$(cat /run/secrets/maevsi_sqitch-target)"
-
 if [ "$NODE_ENV" != "production" ]; then
     pnpm config set store-dir "/srv/.pnpm-store"
     pnpm install
