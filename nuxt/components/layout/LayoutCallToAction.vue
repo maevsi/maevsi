68f52f3080c7ac718fe62795402f47ff7377a1fc
Jonas Thelemann <e-mail@jonas-thelemann.de>
2023-05-13 21:54:21 +0200

    fix(layout-call-to-action): add gap

--- nuxt/components/layout/LayoutCallToAction.vue
+++ nuxt/components/layout/LayoutCallToAction.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex flex-col items-center">
+  <div class="flex flex-col items-center gap-2">
     <span>{{ callToActionDescription }}</span>
     <ButtonColored
       :aria-label="callToAction"
