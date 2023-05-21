3acdebe9c2b714938d271ed6e4087124236e4ee0
Jonas Thelemann <e-mail@jonas-thelemann.de>
2023-05-21 22:40:47 +0200

    chore(cypress): update tests

--- nuxt/cypress/e2e/taskAccountPasswordReset.cy.ts
+++ nuxt/cypress/e2e/taskAccountPasswordReset.cy.ts
@@ -1,26 +1,39 @@
-import { CYPRESS_BASE_URL } from '~/utils/constants'
+import { TIMEZONE_COOKIE_NAME } from '~/utils/constants'
+import {
+  COOKIE_CONTROL_DEFAULT,
+  TIMEZONE_DEFAULT,
+} from '~/cypress/utils/constants'
 
 describe('task account password reset page', () => {
-  context('page load', () => {
-    // TODO: mock data
-    // it('loads the page successfully if reset code is valid', () => {
-    //   cy.request({
-    //     url: '/task/account/password/reset?code=valid',
-    //     followRedirect: false,
-    //   }).then((resp) => {
-    //     expect(resp.status).to.equal(200)
-    //     expect(resp.redirectedToUrl).to.equal(undefined)
-    //   })
-    // })
+  beforeEach(() => {
+    cy.setCookie(TIMEZONE_COOKIE_NAME, TIMEZONE_DEFAULT)
+    cy.setCookie('ncc_c', COOKIE_CONTROL_DEFAULT)
+  })
 
-    it('redirects to index if reset code is invalid', () => {
-      cy.request({
-        url: '/task/account/password/reset?code=invalid',
-        followRedirect: false,
-      }).then((resp) => {
-        expect(resp.status).to.equal(302)
-        expect(resp.redirectedToUrl).to.equal(`${CYPRESS_BASE_URL}/`)
+  // TODO: mock data
+  // context('page load', () => {
+  // it('loads the page successfully if reset code is valid', () => {
+  //   cy.request({
+  //     url: '/task/account/password/reset?code=valid',
+  //     followRedirect: false,
+  //   }).then((resp) => {
+  //     expect(resp.status).to.equal(200)
+  //     expect(resp.redirectedToUrl).to.equal(undefined)
+  //   })
+  // })
+  // })
+
+  context('visual regression', () => {
+    it('looks as before', () => {
+      cy.visit({
+        url: '/task/account/password/reset',
+        failOnStatusCode: false,
       })
+      cy.get('[data-is-loading="false"]').should('be.visible')
+      cy.get('[data-testid="nuxt-cookie-control-control-button"]').should(
+        'be.visible'
+      )
+      cy.compareSnapshot('index')
     })
   })
 })
--- nuxt/cypress/e2e/taskEventCreate.cy.ts
+++ nuxt/cypress/e2e/taskEventCreate.cy.ts
@@ -1,17 +1,23 @@
-import { CYPRESS_BASE_URL } from '~/utils/constants'
+import { TIMEZONE_COOKIE_NAME } from '~/utils/constants'
+import {
+  COOKIE_CONTROL_DEFAULT,
+  TIMEZONE_DEFAULT,
+} from '~/cypress/utils/constants'
 
 describe('task event create page', () => {
-  context('page load', () => {
-    it('redirects to sign-in when not signed in', () => {
-      cy.request({
-        url: '/task/event/create',
-        followRedirect: false,
-      }).then((resp) => {
-        expect(resp.status).to.equal(302)
-        expect(resp.redirectedToUrl).to.equal(
-          `${CYPRESS_BASE_URL}/task/account/sign-in?to=/task/event/create`
-        )
-      })
+  beforeEach(() => {
+    cy.setCookie(TIMEZONE_COOKIE_NAME, TIMEZONE_DEFAULT)
+    cy.setCookie('ncc_c', COOKIE_CONTROL_DEFAULT)
+  })
+
+  context('visual regression', () => {
+    it('looks as before', () => {
+      cy.visit('/task/event/create')
+      cy.get('[data-is-loading="false"]').should('be.visible')
+      cy.get('[data-testid="nuxt-cookie-control-control-button"]').should(
+        'be.visible'
+      )
+      cy.compareSnapshot('index')
     })
   })
 })
