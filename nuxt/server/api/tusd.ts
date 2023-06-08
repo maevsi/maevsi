b6570710ea8b8cf8cd4498d0ddd9b03693e8aa28
Jonas Thelemann <e-mail@jonas-thelemann.de>
2023-06-08 22:25:54 +0200

    fix(upload): delete by bucket key only

--- nuxt/server/api/tusd.ts
+++ nuxt/server/api/tusd.ts
@@ -155,15 +155,20 @@ const tusdDelete = async (event: H3Event) => {
     )
   }
 
-  const storageKey = queryRes.rows[0] ? queryRes.rows[0].storage_key : null
+  const storageKey = (
+    queryRes.rows[0] ? queryRes.rows[0].storage_key : undefined
+  ) as string | undefined
 
-  if (storageKey !== null) {
-    const httpResp = await fetch('http://tusd:1080/files/' + storageKey, {
-      headers: {
-        'Tus-Resumable': '1.0.0',
-      },
-      method: 'DELETE',
-    })
+  if (storageKey) {
+    const httpResp = await fetch(
+      'http://tusd:1080/files/' + storageKey.split('+')[0], // split is necessary for Scaleway's object storage (deletion by bucket key only without suffix)
+      {
+        headers: {
+          'Tus-Resumable': '1.0.0',
+        },
+        method: 'DELETE',
+      }
+    )
 
     if (httpResp.status === 204) {
       await deleteUpload(event, uploadId, storageKey)
