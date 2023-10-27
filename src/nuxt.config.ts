c2af497a48d645ea6a94e22443f5fdeff0627497
Jonas Thelemann <e-mail@jonas-thelemann.de>
2023-10-27 11:56:38 +0200

    refactor: add nuxt security

--- src/nuxt.config.ts
+++ src/nuxt.config.ts
@@ -6,6 +6,7 @@ import { sentryVitePlugin } from '@sentry/vite-plugin'
 
 import { tryResolveModule } from '@nuxt/kit'
 import type { Nuxt } from '@nuxt/schema'
+// import { defu } from 'defu'
 import colors from 'tailwindcss/colors'
 
 import {
@@ -87,6 +88,35 @@ export default defineNuxtConfig({
         nuxt.options.alias[module] = await resolve(module, nuxt)
       }
     }, // TODO: remove after next update following 2023-10-20 (https://github.com/nuxt-modules/i18n/releases, https://github.com/nuxt/nuxt/releases)
+    // nuxt-security: remove invalid `'none'`s and duplicates
+    (_options, nuxt) => {
+      const nuxtConfigSecurity = nuxt.options.security
+
+      if (
+        typeof nuxtConfigSecurity.headers !== 'boolean' &&
+        nuxtConfigSecurity.headers.contentSecurityPolicy &&
+        typeof nuxtConfigSecurity.headers.contentSecurityPolicy !== 'boolean' &&
+        typeof nuxtConfigSecurity.headers.contentSecurityPolicy !== 'string'
+      ) {
+        for (const [key, value] of Object.entries(
+          nuxtConfigSecurity.headers.contentSecurityPolicy,
+        )) {
+          if (!Array.isArray(value)) continue
+
+          const valueFiltered = value.filter((x) => x !== "'none'")
+
+          if (valueFiltered.length) {
+            ;(
+              nuxtConfigSecurity.headers.contentSecurityPolicy as Record<
+                string,
+                any
+              >
+            )[key] = [...new Set(valueFiltered)]
+          }
+        }
+      }
+    },
+    'nuxt-security',
   ],
   nitro: {
     compressPublicAssets: true,
@@ -471,6 +501,153 @@ export default defineNuxtConfig({
   linkChecker: {
     enabled: false,
     failOnError: true,
+  },
+  security: {
+    headers: {
+      contentSecurityPolicy: false,
+      // if (isHttps(event.node.req)) {
+      //   appendHeader(event, 'Cross-Origin-Opener-Policy', 'same-origin')
+      //   appendHeader(event, 'Origin-Agent-Cluster', '?1')
+      // }
+      // contentSecurityPolicy: defu(
+      //   {
+      //     // Cloudflare
+      //     ...(process.env.NODE_ENV === 'production'
+      //       ? {
+      //           'connect-src': ["'self'"], // `${SITE_URL}/cdn-cgi/rum`
+      //           'script-src-elem': ['https://static.cloudflareinsights.com'],
+      //         }
+      //       : {}),
+      //   },
+      //   {
+      //     // Google Analytics 4 (https://developers.google.com/tag-platform/tag-manager/web/csp)
+      //     'connect-src': [
+      //       'https://*.analytics.google.com',
+      //       'https://*.google-analytics.com',
+      //       'https://*.googletagmanager.com',
+      //     ],
+      //     'img-src': [
+      //       'https://*.google-analytics.com',
+      //       'https://*.googletagmanager.com',
+      //     ],
+      //     'script-src-elem': ['https://*.googletagmanager.com'],
+      //   },
+      //   {
+      //     // vio
+      //     'connect-src': ["'self'"], // `${SITE_URL}/api/healthcheck`
+      //     'manifest-src': [`${SITE_URL}/site.webmanifest`],
+      //     'script-src-elem': [
+      //       'https://polyfill.io/v3/polyfill.min.js', // ESLint plugin compat
+      //     ],
+      //   },
+      //   {
+      //     // @nuxt/devtools
+      //     ...(process.env.NODE_ENV === 'development'
+      //       ? {
+      //           'frame-src': [
+      //             'http://localhost:3000/__nuxt_devtools__/client/',
+      //           ],
+      //         }
+      //       : {}),
+      //   },
+      //   {
+      //     // nuxt-link-checker
+      //     ...(process.env.NODE_ENV === 'development'
+      //       ? {
+      //           'connect-src': ["'self'"], // 'http://localhost:3000/api/__link_checker__/inspect'
+      //         }
+      //       : {}),
+      //   },
+      //   {
+      //     // nuxt-og-image
+      //     ...(process.env.NODE_ENV === 'development'
+      //       ? {
+      //           'font-src': ['https://fonts.gstatic.com/s/inter/'],
+      //           'frame-ancestors': ["'self'"],
+      //           'frame-src': ["'self'"],
+      //           'script-src-elem': ['https://cdn.tailwindcss.com/'],
+      //           'style-src': [
+      //             // TODO: replace with `style-src-elem` once Webkit supports it
+      //             'https://cdn.jsdelivr.net/npm/gardevoir https://fonts.googleapis.com/css2',
+      //           ],
+      //         }
+      //       : {}),
+      //   },
+      //   {
+      //     // nuxt-simple-sitemap
+      //     'script-src-elem': [`${SITE_URL}/__sitemap__/style.xsl`],
+      //   },
+      //   {
+      //     // nuxt
+      //     'connect-src': [
+      //       ...(process.env.NODE_ENV === 'development'
+      //         ? [
+      //             'http://localhost:3000/_nuxt/', // hot reload
+      //             'https://localhost:3000/_nuxt/', // hot reload
+      //             'ws://localhost:3000/_nuxt/', // hot reload
+      //             'wss://localhost:3000/_nuxt/', // hot reload
+      //           ]
+      //         : ["'self'"]), // build metadata and payloads
+      //     ],
+      //     'img-src': [
+      //       "'self'", // TODO: replace with `"'nonce-{{nonce}}'",`
+      //       'data:', // external link icon
+      //     ],
+      //     'script-src-elem': ["'nonce-{{nonce}}'"],
+      //     'style-src': [
+      //       // TODO: replace with `style-src-elem` once Webkit supports it
+      //       "'self'", // TODO: replace with `"'nonce-{{nonce}}'",` (https://github.com/vitejs/vite/pull/11864)
+      //       "'unsafe-inline'", // TODO: replace with `"'nonce-{{nonce}}'",` (https://github.com/vitejs/vite/pull/11864)
+      //     ],
+      //   },
+      //   {
+      //     // nitro
+      //     'connect-src': ["'self'"] /* swagger
+      //     'http://localhost:3000/_nitro/openapi.json',
+      //     'http://localhost:3000/_nitro/swagger', */,
+      //     'script-src-elem': [
+      //       'https://cdn.jsdelivr.net/npm/', // swagger // TODO: increase precision (https://github.com/unjs/nitro/issues/1757)
+      //     ],
+      //     'style-src': [
+      //       'https://cdn.jsdelivr.net/npm/', // swagger // TODO: increase precision (https://github.com/unjs/nitro/issues/1757)
+      //     ],
+      //   },
+      //   {
+      //     // base
+      //     'base-uri': ["'none'"], // does not fallback to `default-src`
+      //     'child-src': [],
+      //     'connect-src': [],
+      //     'default-src': ["'none'"],
+      //     'font-src': [],
+      //     'form-action': ["'none'"], // does not fallback to `default-src`
+      //     'frame-ancestors': ["'none'"], // does not fallback to `default-src`
+      //     'frame-src': [],
+      //     'img-src': [],
+      //     'media-src': [],
+      //     'navigate-to': [],
+      //     'object-src': [],
+      //     'prefetch-src': [],
+      //     'report-to': [],
+      //     'report-uri': [],
+      //     // TODO: evaluate header (https://github.com/maevsi/maevsi/issues/830) // https://stackoverflow.com/questions/62081028/this-document-requires-trustedscripturl-assignment
+      //     // 'require-trusted-types-for': ["'script'"], // csp-evaluator
+      //     sandbox: [],
+      //     'script-src': [],
+      //     'script-src-attr': [],
+      //     'script-src-elem': [],
+      //     'style-src': [],
+      //     'style-src-attr': [],
+      //     'style-src-elem': [],
+      //     'upgrade-insecure-requests': false, // TODO: set to `process.env.NODE_ENV === 'production'` or `true` when tests run on https
+      //     'worker-src': [],
+      //   },
+      // ),
+      crossOriginEmbedderPolicy: false, // https://stackoverflow.com/questions/71904052/getting-notsameoriginafterdefaultedtosameoriginbycoep-error-with-helmet
+      xXSSProtection: '1; mode=block', // TODO: set back to `0` once CSP does not use `unsafe-*` anymore (https://github.com/maevsi/maevsi/issues/1047)
+    },
+    nonce: {
+      enabled: true,
+    },
   },
   seo: {
     splash: false,
--- src/server/middleware/headers.ts
+++ src/server/middleware/headers.ts
@@ -1,48 +1,18 @@
-import { appendHeader, defineEventHandler, type H3Event } from 'h3'
-import isHttps from 'is-https'
+import { getCspAsString } from '~/utils/util'
 
-import { TIMEZONE_HEADER_KEY } from '~/utils/constants'
-import { getCspAsString, getTimezone } from '~/utils/util'
+export default defineEventHandler((event) => {
+  const runtimeConfig = useRuntimeConfig()
 
-export default defineEventHandler(async (event) => {
+  appendHeader(event, 'Content-Security-Policy', getCspAsString(event)) // TODO: migrate to nuxt-security header (https://github.com/maevsi/maevsi/issues/1416)
 
-  appendHeader(event, 'Content-Security-Policy', getCspAsString(event))
-  // appendHeader(event, 'Cross-Origin-Embedder-Policy', 'require-corp') // https://stackoverflow.com/questions/71904052/getting-notsameoriginafterdefaultedtosameoriginbycoep-error-with-helmet
-
-  if (isHttps(event.node.req)) {
-    appendHeader(event, 'Cross-Origin-Opener-Policy', 'same-origin')
-  }
-  appendHeader(event, 'Cross-Origin-Resource-Policy', 'same-origin')
-  // appendHeader(event, 'Expect-CT', 'max-age=0') // deprecated (https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Expect-CT)
   appendHeader(
     event,
     'NEL',
     '\'{"report_to":"default","max_age":31536000,"include_subdomains":true}\'',
   )
-
-  if (isHttps(event.node.req)) {
-    appendHeader(event, 'Origin-Agent-Cluster', '?1')
-  }
-
-  appendHeader(event, 'Permissions-Policy', '')
-  appendHeader(event, 'Referrer-Policy', 'no-referrer')
   appendHeader(
     event,
     'Report-To',
-    '\'{"group":"default","max_age":31536000,"endpoints":[{"url":"https://dargmuesli.report-uri.com/a/d/g"}],"include_subdomains":true}\'',
+    `'{"group":"default","max_age":31536000,"endpoints":[{"url":"https://${runtimeConfig.public.sentry.host}/api/${runtimeConfig.public.sentry.project.client.id}/security/?sentry_key=${runtimeConfig.public.sentry.project.client.publicKey}&sentry_environment=${runtimeConfig.public.vio.environment}&sentry_release=${runtimeConfig.public.vio.releaseName}"}],"include_subdomains":true}'`,
   )
-  appendHeader(event, 'X-Content-Type-Options', 'nosniff')
-  appendHeader(event, 'X-DNS-Prefetch-Control', 'off')
-  appendHeader(event, 'X-Download-Options', 'noopen')
-  appendHeader(event, 'X-Frame-Options', 'SAMEORIGIN')
-  appendHeader(event, 'X-Permitted-Cross-Domain-Policies', 'none')
-  appendHeader(event, 'X-XSS-Protection', '1; mode=block') // TODO: set back to `0` once CSP does not use `unsafe-*` anymore (https://github.com/maevsi/maevsi/issues/1047)
-
-  if (process.env.NODE_ENV === 'production') {
-    appendHeader(
-      event,
-      'Strict-Transport-Security',
-      'max-age=31536000; includeSubDomains; preload',
-    )
-  }
 })
