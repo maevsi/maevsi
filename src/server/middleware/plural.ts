ca436cb4b3d0797d014c89301a4eb9379e9497e8
Jonas Thelemann <e-mail@jonas-thelemann.de>
2023-10-31 07:13:39 +0100

    fix(middleware): correct plural regex

--- src/server/middleware/plural.ts
+++ src/server/middleware/plural.ts
@@ -1,23 +1,45 @@
 export default defineEventHandler((event) => {
   const requestPath = event.path
 
-  if (requestPath.match(/^\/(account|contact|event|task|upload)\//)) {
+  if (
+    requestPath.match(
+      /^\/(de\/)?(account|contact|event|task|upload)([^s]|$)(.*)/,
+    )
+  ) {
     return sendRedirect(
       event,
       requestPath.replace(
-        /^\/(account|contact|event|task|upload)(.*)/,
-        (_a, b, c) => `/${b}s${c}`,
+        /^\/(de\/)?(account|contact|event|task|upload)([^s]|$)(.*)/,
+        (_a, b, c, d, e) => `/${b || ''}${c || ''}s${d || ''}${e || ''}`,
       ),
       302,
     )
   }
 
-  if (requestPath.match(/^\/tasks\/(account|event)\//)) {
+  if (requestPath.match(/^\/(de\/)?tasks\/(account|event)([^s]|$)(.*)/)) {
     return sendRedirect(
       event,
       requestPath.replace(
-        /^\/tasks\/(account|event)(.*)/,
-        (_a, b, c) => `/tasks/${b}s${c}`,
+        /^\/(de\/)?tasks\/(account|event)(.*)/,
+        (_a, b, c, d, e) => `/${b || ''}tasks/${c || ''}s${d || ''}${e || ''}`,
+      ),
+      302,
+    )
+  }
+
+  if (
+    requestPath.match(
+      /^\/(de\/)?events\/([^/]+)\/([^/]+)\/(attendance|invitation)([^s]|$)(.*)/,
+    )
+  ) {
+    return sendRedirect(
+      event,
+      requestPath.replace(
+        /^\/(de\/)?events\/([^/]+)\/([^/]+)\/(attendance|invitation)([^s]|$)(.*)/,
+        (_a, b, c, d, e, f, g) =>
+          `/${b || ''}events/${c || ''}/${d || ''}/${e || ''}s${f || ''}${
+            g || ''
+          }`,
       ),
       302,
     )
