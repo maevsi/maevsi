cd605c515e1e786f41f685c11dbda0158fa8b805
Jonas Thelemann <e-mail@jonas-thelemann.de>
2023-11-02 05:49:57 +0100

    refactor(timezone): restructure server getter

--- src/server/utils/timezone.ts
+++ src/server/utils/timezone.ts
@@ -3,13 +3,23 @@ import { H3Event } from 'h3'
 import { TIMEZONE_COOKIE_NAME } from '~/utils/constants'
 
 // TODO: rename to `getTimezone` (https://github.com/nuxt/cli/issues/266)
-export const getTimezoneServer = async (event: H3Event) =>
-  event.context.$timezone ||
-  getCookie(event, TIMEZONE_COOKIE_NAME) ||
-  (event.node.req.headers['x-real-ip'] &&
-    !Array.isArray(event.node.req.headers['x-real-ip']))
-    ? await getTimezoneByIpApi(event.node.req.headers['x-real-ip'] as string)
-    : undefined
+export const getTimezoneServer = async (event: H3Event) => {
+  const timezoneBySsr = event.context.$timezone
+
+  if (timezoneBySsr) return timezoneBySsr
+
+  const timezoneByCookie = getCookie(event, TIMEZONE_COOKIE_NAME)
+
+  if (timezoneByCookie) return timezoneByCookie
+
+  const ip = event.node.req.headers['x-real-ip']
+
+  if (ip && !Array.isArray(ip)) {
+    const timezoneByIpApi = await getTimezoneByIpApi(ip)
+
+    if (timezoneByIpApi) return timezoneByIpApi
+  }
+}
 
 export const getTimezoneByIpApi = async (ip: string) => {
   const ipApiResult = await $fetch<{ timezone: string }>(
