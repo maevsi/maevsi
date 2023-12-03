78b700b21a88b9e337046e7d90976f0d2cb6e169
Jonas Thelemann <e-mail@jonas-thelemann.de>
2023-12-03 05:44:48 +0100

    refactor(breadcrumbs): merge ui

--- src/components/layout/LayoutBreadcrumbs.vue
+++ src/components/layout/LayoutBreadcrumbs.vue
@@ -1,5 +1,18 @@
 <template>
-  <SBreadcrumb :items="items" :ui="BREADCRUMBS_UI">
+  <SBreadcrumb
+    :items="items"
+    :ui="{
+      ol: 'flex gap-2 text-gray-600 dark:text-gray-400',
+      li: 'flex items-center whitespace-nowrap leading-loose group',
+      item: {
+        icon: 'h-5 w-5',
+        iconWithLabel: 'mr-1',
+        default: ITEM_UI,
+        current: ITEM_UI,
+        separator: 'ml-2 text-gray-400 dark:text-gray-600',
+      },
+    }"
+  >
     <template #icon>
       <IHeroiconsHome />
     </template>
@@ -13,4 +26,8 @@ export interface Props {
   items: (string | BreadcrumbItemProps)[]
 }
 withDefaults(defineProps<Props>(), {})
+
+// data
+const ITEM_UI =
+  'flex items-center focus:underline hover:underline group-hover:opacity-80 transition-all aria-[current]:font-bold'
 </script>
