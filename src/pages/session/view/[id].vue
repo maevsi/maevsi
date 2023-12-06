dec9c0b01b71e07aec5cfaa2eb0b1c255a0aa9c8
Jonas Thelemann <e-mail@jonas-thelemann.de>
2023-12-06 03:39:50 +0100

    feat(session): add notification test

--- src/pages/session/view/[id].vue
+++ src/pages/session/view/[id].vue
@@ -24,41 +24,153 @@
         </p>
       </section>
       <section class="flex flex-col gap-4">
+        <h2>{{ t('codes') }}</h2>
+        <div class="flex flex-col gap-4 lg:flex-row">
+          <Card class="flex-1">
+            <div v-if="store.jwtDecoded?.invitations">
+              <p>
+                {{ t('codesEntered') }}
+              </p>
+              <ul class="list-disc">
+                <li
+                  v-for="invitationId in store.jwtDecoded?.invitations"
+                  :key="invitationId"
+                >
+                  {{ invitationId }}
+                </li>
+              </ul>
+            </div>
+            <p v-else>
+              {{ t('codesEnteredNone') }}
+            </p>
+          </Card>
+          <div class="flex-1">
+            <div class="flex flex-col">
+              <ButtonEventUnlock />
+            </div>
+          </div>
+        </div>
+      </section>
+      <section class="flex flex-col gap-4">
         <h2>{{ t('userAgentString') }}</h2>
         <div>
           {{ userAgentString }}
         </div>
       </section>
-      <!-- TODO: move the following to /invitations -->
       <section class="flex flex-col gap-4">
-        <h2>{{ t('codes') }}</h2>
-        <div v-if="store.jwtDecoded?.invitations">
-          <p>
-            {{ t('codesEntered') }}
-          </p>
-          <ul class="list-disc">
-            <li
-              v-for="invitationId in store.jwtDecoded?.invitations"
-              :key="invitationId"
-            >
-              {{ invitationId }}
-            </li>
-          </ul>
+        <h2>{{ t('notification') }}</h2>
+        <div class="flex flex-col gap-4 lg:flex-row">
+          <section class="flex-1">
+            <Card class="flex flex-col gap-1">
+              <div class="flex gap-2">
+                <IHeroiconsQuestionMarkCircle
+                  v-if="isNavigatorHavingPermissions === undefined"
+                />
+                <IHeroiconsCheckCircle
+                  v-else-if="isNavigatorHavingPermissions"
+                  class="text-green-600 dark:text-green-500"
+                />
+                <IHeroiconsXCircle
+                  v-else
+                  class="text-red-600 dark:text-red-500"
+                />
+                <span>
+                  {{ t('hasNavigatorPermissions') }}
+                </span>
+              </div>
+              <div class="flex gap-2">
+                <IHeroiconsQuestionMarkCircle
+                  v-if="isNavigatorHavingServiceWorker === undefined"
+                />
+                <IHeroiconsCheckCircle
+                  v-else-if="isNavigatorHavingServiceWorker"
+                  class="text-green-600 dark:text-green-500"
+                />
+                <IHeroiconsXCircle
+                  v-else
+                  class="text-red-600 dark:text-red-500"
+                />
+                <span>
+                  {{ t('hasNavigatorServiceWorkers') }}
+                </span>
+              </div>
+              <div class="flex gap-2">
+                <IHeroiconsQuestionMarkCircle
+                  v-if="isWindowHavingNotification === undefined"
+                />
+                <IHeroiconsCheckCircle
+                  v-else-if="isWindowHavingNotification"
+                  class="text-green-600 dark:text-green-500"
+                />
+                <IHeroiconsXCircle
+                  v-else
+                  class="text-red-600 dark:text-red-500"
+                />
+                <span>
+                  {{ t('hasWindowNotification') }}
+                </span>
+              </div>
+              <div class="flex gap-2">
+                <IHeroiconsQuestionMarkCircle
+                  v-if="permissionState === undefined"
+                />
+                <IHeroiconsCheckCircle
+                  v-else-if="permissionState === 'granted'"
+                  class="text-green-600 dark:text-green-500"
+                />
+                <IHeroiconsQuestionMarkCircle
+                  v-else-if="permissionState === 'prompt'"
+                  class="text-blue-600 dark:text-blue-400"
+                />
+                <IHeroiconsXCircle
+                  v-else-if="permissionState === 'denied'"
+                  class="text-red-600 dark:text-red-500"
+                />
+                <IHeroiconsBugAnt v-else class="text-red-500" />
+                <span>
+                  {{ t('notificationPermitted') }}
+                </span>
+              </div>
+            </Card>
+          </section>
+          <section class="flex-1">
+            <div class="flex flex-col gap-2">
+              <ButtonColored
+                :aria-label="t('notificationPermit')"
+                :disabled="!isNotificationPermissionRequestPossible"
+                @click="requestNotificationPermissions"
+              >
+                {{ t('notificationPermit') }}
+                <template #prefix>
+                  <IHeroiconsShieldCheck />
+                </template>
+              </ButtonColored>
+              <ButtonColored
+                :aria-label="t('notificationSend')"
+                :disabled="permissionState !== 'granted'"
+                @click="sendNotification"
+              >
+                {{ t('notificationSend') }}
+                <template #prefix>
+                  <IHeroiconsBellAlert />
+                </template>
+              </ButtonColored>
+            </div>
+          </section>
         </div>
-        <p v-else>
-          {{ t('codesEnteredNone') }}
-        </p>
-        <ButtonEventUnlock />
       </section>
     </div>
   </div>
 </template>
 
 <script lang="ts">
+import { consola } from 'consola'
+
 import { usePageBreadcrumb as usePageBreadcrumbHome } from '../../index.vue'
-import type { BreadcrumbItemPropsLocalizedObject } from '~/types/breadcrumbs'
 
 import { type RoutesNamesList } from '@typed-router'
+
+import type { BreadcrumbItemPropsLocalizedObject } from '~/types/breadcrumbs'
 
 const ROUTE_NAME: RoutesNamesList = 'session-view-id'
 
@@ -93,9 +205,31 @@ const breadcrumbItems = defineBreadcrumbItems(
     },
   ]),
 )
+const isNavigatorHavingPermissions = ref<boolean>()
+const isNavigatorHavingServiceWorker = ref<boolean>()
+const isWindowHavingNotification = ref<boolean>()
+const permissionState = ref<PermissionState>()
 const title = t('title')
 
+// methods
+const sendNotification = () =>
+  new Notification('Hey cutie 👋', {
+    body: "It's great to see you!",
+    icon: '/assets/static/logos/maevsi_icon.svg',
+  })
+const requestNotificationPermissions = () => Notification.requestPermission()
+// const requestNotificationPermissions = async () => {
+//   const notificationPermission = await Notification.requestPermission()
+
+//   if (notificationPermission === 'granted') {
+//     sendNotification()
+//   }
+// }
+
 // computations
+const isNotificationPermissionRequestPossible = computed(
+  () => process.client && permissionState.value === 'prompt',
+)
 const sessionExpiryTime = computed(() =>
   store.jwtDecoded?.exp
     ? dateTime(store.jwtDecoded?.exp * 1000).format('llll')
@@ -104,6 +238,30 @@ const sessionExpiryTime = computed(() =>
 const userAgentString = computed(() =>
   requestEvent ? requestEvent.headers.get('user-agent') : navigator.userAgent,
 )
+
+// lifecycle
+onMounted(async () => {
+  isNavigatorHavingPermissions.value = 'permissions' in navigator
+  isNavigatorHavingServiceWorker.value = 'serviceWorker' in navigator
+  isWindowHavingNotification.value = 'Notification' in window
+
+  if (isNavigatorHavingPermissions) {
+    const permissionStatus = await navigator.permissions.query({
+      name: 'notifications',
+    })
+
+    permissionStatus.onchange = () => {
+      consola.log(
+        'User decided to change his seettings. New permission: ' +
+          permissionStatus.state,
+      )
+      permissionState.value = permissionStatus.state
+    }
+  }
+
+  permissionState.value =
+    Notification.permission === 'default' ? 'prompt' : Notification.permission
+})
 
 // initialization
 useHeadDefault({ title })
@@ -115,7 +273,14 @@ de:
   codesEntered: 'Du hast die folgenden Codes eingegeben:'
   codesEnteredNone: Dieser Sitzung sind keine Einladungscodes zugeordnet 😕
   edit: Bearbeiten
+  hasNavigatorPermissions: Navigator hat Berechtigungen
+  hasNavigatorServiceWorkers: Navigator hat Service Worker
+  hasWindowNotification: Fenster hat Benachrichtigung
   metadata: Metadaten
+  notification: Benachrichtigungen
+  notificationPermit: Benachrichtigungen erlauben
+  notificationPermitted: Benachrichtigungen sind erlaubt
+  notificationSend: Benachrichtigung senden
   sessionExpiry: Deine Sitzung läuft am {exp} ab.
   title: Sitzung
   userAgentString: User agent string
@@ -124,7 +289,14 @@ en:
   codesEntered: 'You entered the following codes:'
   codesEnteredNone: There are no invitation codes assigned to this session 😕
   edit: Edit
+  hasNavigatorPermissions: Navigator has permissions
+  hasNavigatorServiceWorkers: Navigator has service workers
+  hasWindowNotification: Window has notification
   metadata: Metadata
+  notification: Notifications
+  notificationPermit: Permit notifications
+  notificationPermitted: Notifications are permitted
+  notificationSend: Send notification
   sessionExpiry: Your session expires on {exp}.
   title: Session
   userAgentString: User agent string
