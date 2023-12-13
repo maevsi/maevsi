5169f6fd0f9960c5c1d9bbbfc6c275b461289932
Jonas Thelemann <e-mail@jonas-thelemann.de>
2023-12-13 11:16:50 +0100

    test(search): disable visual regression test temporarily

--- tests/e2e/specs/pages/task/taskSearch.spec.ts
+++ tests/e2e/specs/pages/task/taskSearch.spec.ts
@@ -1,6 +1,6 @@
-import { testPageLoad, testVisualRegression } from '#tests/e2e/utils/tests'
+import { testPageLoad } from '#tests/e2e/utils/tests'
 
 const PAGE_PATH = '/search'
 
 testPageLoad(PAGE_PATH)
-testVisualRegression(PAGE_PATH)
+// testVisualRegression(PAGE_PATH)
