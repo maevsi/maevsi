0a170ef57868680c26023503823bd81c1c5ca41a
Jonas Thelemann <e-mail@jonas-thelemann.de>
2023-12-13 14:14:56 +0100

    fix(layout-bottom-navigation): correct pointer events

--- src/components/layout/LayoutBottomNavigation.vue
+++ src/components/layout/LayoutBottomNavigation.vue
@@ -1,7 +1,9 @@
 <template>
-  <div class="sticky bottom-4 flex justify-center pt-8 lg:hidden">
+  <div
+    class="pointer-events-none sticky bottom-4 flex justify-center pt-8 lg:hidden"
+  >
     <div
-      class="flex gap-1 rounded-xl bg-blue-200 px-4 py-2 shadow-lg dark:bg-background-darken sm:gap-4"
+      class="pointer-events-auto flex gap-1 rounded-xl bg-blue-200 px-4 py-2 shadow-lg dark:bg-background-darken sm:gap-4"
     >
       <LayoutMenu />
     </div>
