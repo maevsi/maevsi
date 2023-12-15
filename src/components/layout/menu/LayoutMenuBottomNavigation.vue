fecbf025f0e71b352ace31bbf0908b5bd6fc957a
Jonas Thelemann <e-mail@jonas-thelemann.de>
2023-12-15 01:40:14 +0100

    feat(layout-menu-bottom-navigation): add ring

--- src/components/layout/menu/LayoutMenuBottomNavigation.vue
+++ src/components/layout/menu/LayoutMenuBottomNavigation.vue
@@ -3,7 +3,7 @@
     class="pointer-events-none sticky bottom-4 flex justify-center pt-8 lg:hidden"
   >
     <div
-      class="pointer-events-auto flex gap-1 rounded-xl bg-blue-200 px-4 py-2 shadow-lg dark:bg-background-darken sm:gap-4"
+      class="pointer-events-auto flex gap-1 rounded-xl bg-blue-200 px-4 py-2 shadow-lg ring-1 ring-zinc-600 dark:bg-background-darken sm:gap-4"
     >
       <LayoutMenu />
     </div>
