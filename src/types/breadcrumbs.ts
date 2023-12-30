1eb0f93a584aa6328d858c63629bcb42e3a7d5de
Jonas Thelemann <e-mail@jonas-thelemann.de>
2023-12-30 02:33:52 +0100

    fix(breadcrumbs): correct types

--- src/types/breadcrumbs.ts
+++ src/types/breadcrumbs.ts
@@ -3,9 +3,9 @@ import type { TypedLocalePathParameter } from '@typed-router/__paths'
 
 export type BreadcrumbItemPropsLocalizedObject = Omit<
   BreadcrumbItemProps,
-  'aria-label' | 'label' | 'to'
+  'ariaLabel' | 'label' | 'to'
 > & {
-  'aria-label'?: { [Key in LOCALE_CODES]: string } | string
+  ariaLabel?: { [Key in LOCALE_CODES]: string } | string
   label?: { [Key in LOCALE_CODES]: string } | string
   to: TypedLocalePathParameter<never>
 }
