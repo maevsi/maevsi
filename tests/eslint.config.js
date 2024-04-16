393318cf108686eee5d45e476e898aab8ac37d2d
Jonas Thelemann <e-mail@jonas-thelemann.de>
2024-04-16 21:23:37 +0200

    chore(lint): remove plugin yml

--- tests/eslint.config.js
+++ tests/eslint.config.js
@@ -1,18 +1,15 @@
 // @ts-check
 import eslint from '@eslint/js'
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
-import eslintPluginYml from 'eslint-plugin-yml'
 import tseslint from 'typescript-eslint'
 
 export default tseslint.config(
   eslint.configs.recommended,
   ...tseslint.configs.recommended,
   eslintPluginPrettierRecommended,
-  ...eslintPluginYml.configs['flat/recommended'],
   {
     rules: {
       'no-console': 'error',
-      'yml/quotes': ['error', { prefer: 'double' }],
     },
   },
 )
