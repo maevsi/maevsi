7672a7e768d7dbaa6ee90983b03462635bbb5a9e
Jonas Thelemann <e-mail@jonas-thelemann.de>
2024-04-17 23:06:54 +0200

    refactor(lint): rename variable

--- tests/eslint.config.js
+++ tests/eslint.config.js
@@ -1,12 +1,12 @@
 // @ts-check
 import eslint from '@eslint/js'
-import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
+import prettierRecommended from 'eslint-plugin-prettier/recommended'
 import tseslint from 'typescript-eslint'
 
 export default tseslint.config(
   eslint.configs.recommended,
   ...tseslint.configs.recommended,
-  eslintPluginPrettierRecommended,
+  prettierRecommended,
   {
     rules: {
       'no-console': 'error',
