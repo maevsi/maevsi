f00c0703479869ec3ef3f1b671ee15c8f8ddcb83
Jonas Thelemann <e-mail@jonas-thelemann.de>
2024-05-07 11:26:04 +0200

    chore(platform): remove cookie deletion

--- src/server/middleware/platform.ts
+++ src/server/middleware/platform.ts
@@ -1,6 +1,4 @@
 export default defineEventHandler(async (event) => {
-  deleteCookie(event, 'maevsi-platform') // TODO: remove after some time, this is only for migration
-
   const maevsiPlatformHeader = getHeader(event, 'maevsi-platform')
 
   if (maevsiPlatformHeader) {
