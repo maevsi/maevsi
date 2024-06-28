433eab82a318dcaf6aba6c6fa5665265c7a9ce02
Jonas Thelemann <e-mail@jonas-thelemann.de>
2024-06-28 05:18:39 +0200

    fix(polyfill): remove

--- src/composables/head.ts
+++ src/composables/head.ts
@@ -82,6 +82,8 @@ export const useHeadDefault = ({
 }
 
 export const usePolyfills = () => {
+  return // hijacked ⚠️
+
   const polyfills = `https://polyfill.io/v3/polyfill.min.js?features=${POLYFILLS.join(
     '%2C',
   )}&flags=gated`
