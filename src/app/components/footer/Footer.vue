97a431acc72ea8770493782038c34aabc40ee9ea
huzaifaedhi2 <huzaifaejaz09@gmail.com>
2024-12-13 15:14:25 +0100

    fix(account): convert route paths to route names in footer component

--- src/app/components/footer/Footer.vue
+++ src/app/components/footer/Footer.vue
@@ -1,14 +1,15 @@
 <template>
   <footer class="mt-auto py-8">
     <div class="mx-auto flex flex-row justify-center gap-x-20">
-      <AppLink :to="localePath('/legal-notice')" :is-colored="true">
+      <AppLink :to="localePath('legal-notice')" :is-colored="true">
         {{ t('terms') }}
       </AppLink>
 
-      <AppLink :to="localePath('/')" :is-colored="true">
+      <AppLink :to="localePath('index')" :is-colored="true">
         {{ t('imprint') }}
       </AppLink>
-      <AppLink :to="localePath('/privacy-policy')" :is-colored="true">
+
+      <AppLink :to="localePath('privacy-policy')" :is-colored="true">
         {{ t('policy') }}
       </AppLink>
     </div>
