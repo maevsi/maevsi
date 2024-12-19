b241e19619e2b24838cda35bb6ccfe563b4805a0
Jonas Thelemann <e-mail@jonas-thelemann.de>
2024-12-19 19:46:26 +0100

    fix(composables): add missing site url getter

---
+++ src/app/composables/vio.ts
@@ -0,0 +1,9 @@
+export const useSiteUrl = () => {
+  const runtimeConfig = useRuntimeConfig()
+  const siteUrl = runtimeConfig.public.site.url
+
+  return {
+    siteUrl,
+    siteUrlTyped: new URL(siteUrl),
+  }
+}
