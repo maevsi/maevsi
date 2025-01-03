854f9818c2d56fbc0a170966f346d29a0606ce74
Jonas Thelemann <e-mail@jonas-thelemann.de>
2025-01-03 20:07:17 +0100

    fix(shadcn): update paths

--- src/.config/nuxt.ts
+++ src/.config/nuxt.ts
@@ -121,7 +121,7 @@ export default defineNuxtConfig({
   ],
   shadcn: {
     prefix: '',
-    componentDir: 'components/ui',
+    componentDir: 'app/components/scn',
   },
   nitro: {
     compressPublicAssets: true,
