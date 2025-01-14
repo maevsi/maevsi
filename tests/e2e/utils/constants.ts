d9c0eb53cad137548f391a356aa7a2f41c12f128
Jonas Thelemann <e-mail@jonas-thelemann.de>
2025-01-14 08:30:09 +0100

    test(page): check cookie control for readiness

--- tests/e2e/utils/constants.ts
+++ tests/e2e/utils/constants.ts
@@ -14,11 +14,11 @@ export const PAGE_READY = async ({
     isLoading?: boolean
   }
 }) => {
-  // if (!options || options.cookieControl !== false) {
-  //   await expect(
-  //     page.getByRole('button', { name: 'Cookie control' }),
-  //   ).toBeVisible()
-  // }
+  if (!options || options.cookieControl !== false) {
+    await expect(
+      page.getByRole('button', { name: 'Cookie control' }),
+    ).toBeVisible()
+  }
 
   if (!options || options.isLoading !== false) {
     await expect(page.getByTestId('is-loading')).toHaveAttribute(
