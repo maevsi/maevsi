a459983057efcff48617cce4481574cf6f6e8c0f
Jonas Thelemann <e-mail@jonas-thelemann.de>
2025-02-09 07:42:42 +0100

    refactor(style): sort classes

--- src/app/components/layout/LayoutProse.vue
+++ src/app/components/layout/LayoutProse.vue
@@ -1,7 +1,7 @@
 <template>
   <!-- 2xl:prose-2xl -->
   <div
-    class="prose dark:prose-invert prose-sm sm:prose-base lg:prose-lg xl:prose-xl m-auto"
+    class="prose prose-sm m-auto dark:prose-invert sm:prose-base lg:prose-lg xl:prose-xl"
   >
     <slot />
   </div>
