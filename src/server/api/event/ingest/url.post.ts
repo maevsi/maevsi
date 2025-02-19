e9ce65e32cb1b274fcf4ef5e9a9d8714d056fbdb
Jonas Thelemann <e-mail@jonas-thelemann.de>
2025-02-20 00:25:24 +0100

    refactor(open-ai): extract cost formatter

--- src/server/api/event/ingest/url.post.ts
+++ src/server/api/event/ingest/url.post.ts
@@ -1,40 +1,36 @@
+import * as cheerio from 'cheerio'
 import { zodResponseFormat } from 'openai/helpers/zod'
 import { z } from 'zod'
-import * as cheerio from 'cheerio'
 
 const eventIngestUrlPostBodySchema = z.object({
   url: z.string(),
 })
+const eventSchema = z.object({
+  description: z.string(),
+  end: z.string().optional(),
+  is_event: z.boolean(),
+  location: z.string().optional(),
+  name: z.string().optional(),
+  start: z.string().optional(),
+  url: z.string().optional(),
+})
+const prompt = `You are a data extraction specialist responsible for identifying and cataloging event information.
+Only accept html. Only if the html contains event information, extract that information into JSON. For dates, use ISO 8601 and the current year (${new Date().getFullYear()}) if no year is given. In all other cases, set all fields to an empty string and \`is_event\` to \`false\`.`
 
 export default defineEventHandler(async (event) => {
-  const openAi = useOpenAi()
+  const { client: openAiClient, getCompletionCost } = useOpenAi()
   const verifyAuth = await useVerifyAuth()
 
   await verifyAuth()
-
   const body = await getBodySafe({
     event,
     schema: eventIngestUrlPostBodySchema,
   })
 
-  const Event = z.object({
-    description: z.string(),
-    end: z.string().optional(),
-    is_event: z.boolean(),
-    location: z.string().optional(),
-    name: z.string().optional(),
-    start: z.string().optional(),
-    url: z.string().optional(),
-  })
+  const html = await $fetch<string>(body.url)
+  const htmlParsed = cheerio.load(html)
 
-  const response = await $fetch(body.url)
-
-  const html = response as string
-  const $ = cheerio.load(html)
-  const prompt = `You are a data extraction specialist responsible for identifying and cataloging event information.
-Only accept html. Only if the html contains event information, extract that information into JSON. For dates, use ISO 8601 and the current year (${new Date().getFullYear()}) if no year is given. In all other cases, set all fields to an empty string and \`is_event\` to \`false\`.`
-
-  const completion = await openAi.beta.chat.completions.parse({
+  const completion = await openAiClient.beta.chat.completions.parse({
     messages: [
       {
         role: 'system',
@@ -46,39 +42,19 @@ Only accept html. Only if the html contains event information, extract that info
           {
             type: 'text',
             text: `\`\`\`
-${$.text()}
+${htmlParsed.text()}
 \`\`\``,
           },
         ],
       },
     ],
     model: 'gpt-4o-mini',
-    response_format: zodResponseFormat(Event, 'event'),
+    response_format: zodResponseFormat(eventSchema, 'event'),
   })
-  const usageJson = completion.usage
-
-  if (!usageJson) return
-
-  // source: https://openai.com/api/pricing/
-  const costs = parseFloat(
-    (
-      (usageJson.prompt_tokens * 0.15 +
-        usageJson.completion_tokens * 0.6 +
-        (usageJson.prompt_tokens_details?.cached_tokens ?? 0) * 0.075) /
-      1e6
-    ).toFixed(7),
-  )
-  const formatter = new Intl.NumberFormat('de-DE', {
-    style: 'currency',
-    currency: 'EUR',
-    minimumFractionDigits: 8,
-  })
-
-  const parsedMessage = completion.choices[0]?.message?.parsed
 
   return {
-    output: parsedMessage,
-    usage: usageJson,
-    costs: formatter.format(costs),
+    output: completion.choices[0]?.message?.parsed,
+    usage: completion.usage,
+    costs: getCompletionCost(completion),
   }
 })
--- src/server/utils/dependencies/openAi.ts
+++ src/server/utils/dependencies/openAi.ts
@@ -1,7 +1,34 @@
+import type { ChatCompletion } from 'openai/resources/index.mjs'
+
+const costFormatter = new Intl.NumberFormat('de-DE', {
+  style: 'currency',
+  currency: 'EUR',
+  minimumFractionDigits: 8,
+})
+
 export const useOpenAi = () => {
   const event = useEvent()
 
   if (!event.context.$openAi) throw createError('openai')
 
-  return event.context.$openAi
+  return {
+    getCompletionCost: (completion: ChatCompletion) => {
+      const usage = completion.usage
+
+      if (!usage) return
+
+      // source: https://openai.com/api/pricing/
+      const costs = parseFloat(
+        (
+          (usage.prompt_tokens * 0.15 +
+            usage.completion_tokens * 0.6 +
+            (usage.prompt_tokens_details?.cached_tokens ?? 0) * 0.075) /
+          1e6
+        ).toFixed(7),
+      )
+
+      return costFormatter.format(costs)
+    },
+    client: event.context.$openAi,
+  }
 }
