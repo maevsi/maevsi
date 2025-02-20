4e3f8ca38975430d5c67aa9f1df00e4903d9821f
Hoang Minh Dang <109755535+HMDank@users.noreply.github.com>
2025-02-20 01:19:49 +0100

    feat(event): create link ingestion functionality (#1775)
    * feat(event-ingest): update event object and prompt
    
    * feat(event-ingest): finish frontend for url endpoint
    
    * feat(event-ingest): remove redundencies
    
    * refactor(open-ai): create client instance in nitro plugin
    
    * chore(event): remove comments in url ingestion
    
    * feat(event): update url ingestion prompt
    
    * refactor(open-ai): extract cost formatter
    
    * feat(event): temporarily disable url ingestion for production
    
    * feat(event): ingest url as given
    
    * chore(test): update snapshots
    
    ---------
    
    Co-authored-by: Jonas Thelemann <e-mail@jonas-thelemann.de>

---
+++ src/server/api/event/ingest/url.post.ts
@@ -0,0 +1,70 @@
+import * as cheerio from 'cheerio'
+import { zodResponseFormat } from 'openai/helpers/zod'
+import { z } from 'zod'
+
+const eventIngestUrlPostBodySchema = z.object({
+  url: z.string(),
+})
+const eventSchema = z.object({
+  description: z.string(),
+  end: z.string().optional(),
+  is_event: z.boolean(),
+  location: z.string().optional(),
+  name: z.string().optional(),
+  start: z.string().optional(),
+})
+const prompt = `You are a data extraction specialist responsible for identifying and cataloging event information.
+Only accept html. Only if the html contains event information, extract that information into JSON. For dates, use ISO 8601 and the current year (${new Date().getFullYear()}) if no year is given. In all other cases, set all fields to an empty string and \`is_event\` to \`false\`.`
+
+export default defineEventHandler(async (event) => {
+  if (!import.meta.dev) {
+    return throwError({
+      code: 503,
+      message:
+        'This endpoint is currently disabled until proper authentication and cost tracking is implemented.',
+    })
+  }
+
+  const { client: openAiClient, getCompletionCost } = useOpenAi()
+  const verifyAuth = await useVerifyAuth()
+
+  await verifyAuth()
+  const body = await getBodySafe({
+    event,
+    schema: eventIngestUrlPostBodySchema,
+  })
+
+  const html = await $fetch<string>(body.url)
+  const htmlParsed = cheerio.load(html)
+
+  const completion = await openAiClient.beta.chat.completions.parse({
+    messages: [
+      {
+        role: 'system',
+        content: prompt,
+      },
+      {
+        role: 'user',
+        content: [
+          {
+            type: 'text',
+            text: `\`\`\`
+${htmlParsed.text()}
+\`\`\``,
+          },
+        ],
+      },
+    ],
+    model: 'gpt-4o-mini',
+    response_format: zodResponseFormat(eventSchema, 'event'),
+  })
+
+  return {
+    output: {
+      ...completion.choices[0]?.message?.parsed,
+      url: body.url,
+    },
+    usage: completion.usage,
+    costs: getCompletionCost(completion),
+  }
+})
--- src/server/types/h3.ts
+++ src/server/types/h3.ts
@@ -1,5 +1,8 @@
+import type OpenAI from 'openai'
+
 declare module 'h3' {
   interface H3EventContext {
+    $openAi?: OpenAI
     $timezone?: string
   }
 }
