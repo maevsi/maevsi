cd7355991d20c1096ed5bcc4bbe00f4825d25047
Jonas Thelemann <e-mail@jonas-thelemann.de>
2025-02-20 06:38:37 +0100

    fix(auth): fully verify json web token in development

--- src/server/api/auth.ts
+++ src/server/api/auth.ts
@@ -2,15 +2,9 @@ export default defineEventHandler(async (event) => {
   const { setJwtCookie, verifyJwt } = await useJsonWebToken()
 
   const headerAuthorization = getRequestHeader(event, 'authorization')
-  let jwt = ''
+  if (!headerAuthorization) return
 
-  if (headerAuthorization) {
-    jwt = headerAuthorization.substring(7)
-
-    if (process.env.NODE_ENV === 'production') {
-      await verifyJwt(jwt)
-    }
-  }
-
+  const jwt = headerAuthorization.substring(7)
+  await verifyJwt(jwt)
   setJwtCookie(jwt)
 })
