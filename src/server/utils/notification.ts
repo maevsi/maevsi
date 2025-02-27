f4001d79af4059d7fcd8881a7175a139021f9557
Jonas Thelemann <e-mail@jonas-thelemann.de>
2025-02-27 05:42:53 +0100

    fix(notification): correct payload

--- src/server/utils/notification.ts
+++ src/server/utils/notification.ts
@@ -70,8 +70,8 @@ type EventInvitationEvent = {
     data: {
       emailAddress: string
       event: MaevsiEvent
-      eventAuthorProfilePictureUploadStorageKey: string
-      eventAuthorUsername: string
+      eventCreatorProfilePictureUploadStorageKey: string
+      eventCreatorUsername: string
       guestId: string
     }
     template: Template
@@ -255,8 +255,8 @@ export const sendEventInvitationMail = async ({
     emailAddress,
     event,
     guestId,
-    eventAuthorProfilePictureUploadStorageKey,
-    eventAuthorUsername,
+    eventCreatorProfilePictureUploadStorageKey,
+    eventCreatorUsername,
   } = payloadCamelCased.data
 
   const res = await (
@@ -266,7 +266,7 @@ export const sendEventInvitationMail = async ({
         event: {
           ...event,
           accountByCreatedBy: {
-            username: eventAuthorUsername,
+            username: eventCreatorUsername,
           },
         },
         guest: {
@@ -342,10 +342,10 @@ export const sendEventInvitationMail = async ({
   await sendEmail({
     limit24h,
     mailOptions: {
-      fromName: eventAuthorUsername,
+      fromName: eventCreatorUsername,
       icalEvent: {
         content: res,
-        filename: eventAuthorUsername + '_' + event.slug + '.ics',
+        filename: eventCreatorUsername + '_' + event.slug + '.ics',
         method: 'request',
       },
       subject: t.subject(event.name),
@@ -355,11 +355,11 @@ export const sendEventInvitationMail = async ({
     props: {
       emailAddress,
       eventAttendanceType,
-      eventAuthorProfileHref: `${siteUrl}/accounts/${eventAuthorUsername}`,
-      eventAuthorProfilePictureSrc: eventAuthorProfilePictureUploadStorageKey
-        ? tusdFilesUrl + eventAuthorProfilePictureUploadStorageKey
+      eventAuthorProfileHref: `${siteUrl}/accounts/${eventCreatorUsername}`,
+      eventAuthorProfilePictureSrc: eventCreatorProfilePictureUploadStorageKey
+        ? tusdFilesUrl + eventCreatorProfilePictureUploadStorageKey
         : 'data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiPz4KPCFET0NUWVBFIHN2ZyAgUFVCTElDICctLy9XM0MvL0RURCBTVkcgMS4xLy9FTicgICdodHRwOi8vd3d3LnczLm9yZy9HcmFwaGljcy9TVkcvMS4xL0RURC9zdmcxMS5kdGQnPgo8c3ZnIHdpZHRoPSI0MDFweCIgaGVpZ2h0PSI0MDFweCIgZW5hYmxlLWJhY2tncm91bmQ9Im5ldyAzMTIuODA5IDAgNDAxIDQwMSIgdmVyc2lvbj0iMS4xIiB2aWV3Qm94PSIzMTIuODA5IDAgNDAxIDQwMSIgeG1sOnNwYWNlPSJwcmVzZXJ2ZSIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj4KPGcgdHJhbnNmb3JtPSJtYXRyaXgoMS4yMjMgMCAwIDEuMjIzIC00NjcuNSAtODQzLjQ0KSI+Cgk8cmVjdCB4PSI2MDEuNDUiIHk9IjY1My4wNyIgd2lkdGg9IjQwMSIgaGVpZ2h0PSI0MDEiIGZpbGw9IiNFNEU2RTciLz4KCTxwYXRoIGQ9Im04MDIuMzggOTA4LjA4Yy04NC41MTUgMC0xNTMuNTIgNDguMTg1LTE1Ny4zOCAxMDguNjJoMzE0Ljc5Yy0zLjg3LTYwLjQ0LTcyLjktMTA4LjYyLTE1Ny40MS0xMDguNjJ6IiBmaWxsPSIjQUVCNEI3Ii8+Cgk8cGF0aCBkPSJtODgxLjM3IDgxOC44NmMwIDQ2Ljc0Ni0zNS4xMDYgODQuNjQxLTc4LjQxIDg0LjY0MXMtNzguNDEtMzcuODk1LTc4LjQxLTg0LjY0MSAzNS4xMDYtODQuNjQxIDc4LjQxLTg0LjY0MWM0My4zMSAwIDc4LjQxIDM3LjkgNzguNDEgODQuNjR6IiBmaWxsPSIjQUVCNEI3Ii8+CjwvZz4KPC9zdmc+Cg==',
-      eventAuthorUsername: eventAuthorUsername,
+      eventAuthorUsername: eventCreatorUsername,
       eventDescription,
       eventDuration: event.end
         ? momentFormatDuration({
