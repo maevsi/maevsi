cd0544e95b03fdcf1699c476afa333f4831917b7
huzaifaedhi2 <huzaifaejaz09@gmail.com>
2025-03-07 02:40:14 +0400

    feat(search-bar): tweak display based on state

--- src/app/components/search/SearchBar.vue
+++ src/app/components/search/SearchBar.vue
@@ -1,6 +1,9 @@
 <template>
   <div
-    class="flex w-full items-center gap-4 rounded-lg bg-white px-4 py-3 shadow"
+    :class="[
+      'hover:bg-faint-weak flex w-full items-center gap-4 rounded-lg px-4 py-3 shadow',
+      { 'ring-accent-line bg-white ring-1': isInputFocused },
+    ]"
   >
     <div class="flex-shrink-0 text-green-800">
       <IHeroiconsMagnifyingGlass class="h-8 w-8" />
@@ -11,30 +14,29 @@
       class="w-full border-none bg-transparent text-xl text-gray-600 placeholder-gray-400 outline-none focus:ring-0 focus:outline-none"
       :placeholder="t('search')"
       type="text"
+      @focus="isInputFocused = true"
+      @blur="isInputFocused = false"
     />
-    <IHeroiconsArrowPath
-      v-if="isSearching"
-      class="h-5 w-5 animate-spin text-gray-400"
+    <IMaevsiColoredCloseIcon
+      v-if="model && model.length > 0"
+      class="h-6 w-6"
+      :alt="t('iconAltClose')"
+      @click="model = ''"
     />
   </div>
 </template>
 
 <script setup lang="ts">
 const { t } = useI18n()
-
-export interface Props {
-  isSearching?: boolean
-}
-withDefaults(defineProps<Props>(), {
-  isSearching: undefined,
-})
-
+const isInputFocused = ref(false)
 const model = defineModel<string | undefined>({ required: true })
 </script>
 
 <i18n lang="yaml">
 de:
+  iconAltClose: Schließen-Symbol
   search: Suche
 en:
+  iconAltClose: Close icon
   search: Search
 </i18n>
