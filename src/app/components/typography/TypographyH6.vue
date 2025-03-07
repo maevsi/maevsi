8b4d540023e98992f425de81ab847ff72e04682a
Jonas Thelemann <e-mail@jonas-thelemann.de>
2025-03-07 03:20:44 +0100

    feat(card): restyle for event

---
+++ src/app/components/typography/TypographyH6.vue
@@ -0,0 +1,18 @@
+<template>
+  <div :class="cn('text-3xl font-semibold', props.class)">
+    <slot />
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { HtmlHTMLAttributes } from 'vue'
+
+import { cn } from '@/utils/shadcn'
+
+const props = withDefaults(
+  defineProps<{ class?: HtmlHTMLAttributes['class'] }>(),
+  {
+    class: undefined,
+  },
+)
+</script>
